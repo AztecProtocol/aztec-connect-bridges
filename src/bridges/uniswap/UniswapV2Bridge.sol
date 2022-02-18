08a4cbe6349a317b9b44a36cd02716ae6a047aac
Joe Andrews <joe@fuuzik.com>
2022-02-18 15:29:42 +0000

    demo

---
+++ src/bridges/uniswap/UniswapV2Bridge.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: GPL-2.0-only
+// Copyright 2020 Spilsbury Holdings Ltd
+pragma solidity >=0.6.10 <=0.8.10;
+pragma experimental ABIEncoderV2;
+
+import { SafeMath } from "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+import { IDefiBridge } from "../../interfaces/IDefiBridge.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import { AztecTypes } from "../../aztec/AztecTypes.sol";
+
+import "../../test/console.sol";
+
+
+contract UniswapV2Bridge is IDefiBridge {
+  using SafeMath for uint256;
+
+  address public immutable rollupProcessor;
+
+  constructor(address _rollupProcessor) public {
+    rollupProcessor = _rollupProcessor;
+  }
+
+  function convert(
+    AztecTypes.AztecAsset memory inputAssetA,
+    AztecTypes.AztecAsset memory inputAssetB,
+    AztecTypes.AztecAsset memory outputAssetA,
+    AztecTypes.AztecAsset memory outputAssetB,
+    uint256 totalInputValue,
+    uint256 interactionNonce,
+    uint64 auxData
+  )
+    external
+    payable
+    override
+    returns (
+      uint256 outputValueA,
+      uint256 outputValueB,
+      bool isAsync
+    )
+  {
+    // // ### INITIALIZATION AND SANITY CHECKS
+    require(msg.sender == rollupProcessor, "ExampleBridge: INVALID_CALLER");
+    outputValueA = totalInputValue;
+    IERC20(inputAssetA.erc20Address).approve(rollupProcessor, totalInputValue);
+  }
+
+
+  function finalise(
+    AztecTypes.AztecAsset calldata inputAssetA,
+    AztecTypes.AztecAsset calldata inputAssetB,
+    AztecTypes.AztecAsset calldata outputAssetA,
+    AztecTypes.AztecAsset calldata outputAssetB,
+    uint256 interactionNonce,
+    uint64 auxData
+  ) external payable override returns (uint256, uint256, bool) {
+    require(false);
+  }
+}
