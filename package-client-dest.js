80f3ce3a11b1cbec4a2449487aef901c4f93ab4e
Joe Andrews <joe@fuuzik.com>
2022-03-21 11:27:35 +0000

    move to dev dependcies in package.json and cleanup

--- package-client-dest.js
+++
@@ -1,8 +0,0 @@
-const { writeFileSync } = require('fs');
-
-const pkg = {
-  name: '@aztec/bridge-clients',
-  version: '0.1.0',
-};
-
-writeFileSync('./client-dest/package.json', JSON.stringify(pkg, null, '  '));
