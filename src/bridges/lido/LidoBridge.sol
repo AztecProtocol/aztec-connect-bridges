65a4bcbe56ad6a9d445ea4b5d8d42c6e7d38b343
Joe Andrews <joe@fuuzik.com>
2022-03-24 17:36:55 +0000

    fix lido bridge

--- src/bridges/lido/LidoBridge.sol
+++ src/bridges/lido/LidoBridge.sol
@@ -35,6 +35,7 @@ interface IWstETH {
     function wrap(uint256 _stETHAmount) external returns (uint256);
     function unwrap(uint256 _wstETHAmount) external returns (uint256);
     function getStETHByWstETH(uint256 _wstETHAmount) external view returns (uint256);
+    function getWstETHByStETH(uint256 _stETHAmount) external view returns (uint256);
 }
 
 interface IRollupProcessor {
