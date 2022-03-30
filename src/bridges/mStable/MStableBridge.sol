afb8296a15fb5461550032f3d32daf62428c4e25
Joe Andrews <joe@creditmint.io>
2022-03-30 18:03:13 +0100

    Update MStableBridge.sol

--- src/bridges/mStable/MStableBridge.sol
+++ src/bridges/mStable/MStableBridge.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-only
-// Copyright 2020 Spilsbury Holdings Ltd
+// Copyright 2022 Spilsbury Holdings Ltd
 pragma solidity >=0.6.10 <=0.8.10;
 pragma experimental ABIEncoderV2;
 
