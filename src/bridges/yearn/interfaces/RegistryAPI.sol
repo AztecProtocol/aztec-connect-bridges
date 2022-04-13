0756c3539d009f8ad44f130e30a52b2c292345e9
John <jjj17@protonmail.com>
2022-04-12 22:06:08 -0400

    add test bridge and yearn bridge

---
+++ src/bridges/yearn/interfaces/RegistryAPI.sol
@@ -0,0 +1,9 @@
+interface RegistryAPI {
+    function governance() external view returns (address);
+
+    function latestVault(address token) external view returns (address);
+
+    function numVaults(address token) external view returns (uint256);
+
+    function vaults(address token, uint256 deploymentId) external view returns (address);
+}
