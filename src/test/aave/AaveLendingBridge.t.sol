c500e2497783aa011399948444a6ad18e0e0afe5
Lasse Herskind <lasse.herskind@gmail.com>
2022-05-10 15:49:05 +0100

    fix: Remove DSTestPlus for hardhat typechain to work

--- src/test/aave/AaveLendingBridge.t.sol
+++ src/test/aave/AaveLendingBridge.t.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import '../../../lib/ds-test/src/test.sol';
-
 import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import {IERC20Metadata} from '@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol';
 
--- src/test/aave/helpers/TestHelper.sol
+++ src/test/aave/helpers/TestHelper.sol
@@ -1,11 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
-import {Vm} from './../../Vm.sol';
-import {DSTestPlus} from '../../../../lib/solmate/src/test/utils/DSTestPlus.sol';
+import {Vm} from '../../../../lib/forge-std/src/Vm.sol';
+import '../../../../lib/forge-std/src/stdlib.sol';
+import '../../../../lib/ds-test/src/test.sol';
 
-contract TestHelper is DSTestPlus {
+import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+
+/**
+ * @notice Helper contract extending the DSTest contract with functions from Solmate and a few extra helper
+ */
+
+contract TestHelper is DSTest {
     Vm internal constant VM = Vm(HEVM_ADDRESS);
 
     function max(uint256 a, uint256 b) internal pure returns (uint256) {
@@ -49,5 +55,45 @@ contract TestHelper is DSTestPlus {
             emit log_named_string('Error', err);
             fail();
         }
+    }
+
+    function assertApproxEq(
+        uint256 a,
+        uint256 b,
+        uint256 maxDelta
+    ) internal virtual {
+        uint256 delta = a > b ? a - b : b - a;
+
+        if (delta > maxDelta) {
+            emit log('Error: a ~= b not satisfied [uint]');
+            emit log_named_uint('  Expected', b);
+            emit log_named_uint('    Actual', a);
+            emit log_named_uint(' Max Delta', maxDelta);
+            emit log_named_uint('     Delta', delta);
+            fail();
+        }
+    }
+
+    function bound(
+        uint256 x,
+        uint256 min,
+        uint256 max
+    ) internal returns (uint256 result) {
+        require(max >= min, 'MAX_LESS_THAN_MIN');
+
+        uint256 size = max - min;
+
+        if (max != type(uint256).max) size++; // Make the max inclusive.
+        if (size == 0) return min; // Using max would be equivalent as well.
+        // Ensure max is inclusive in cases where x != 0 and max is at uint max.
+        if (max == type(uint256).max && x != 0) x--; // Accounted for later.
+
+        if (x < min) x += size * (((min - x) / size) + 1);
+        result = min + ((x - min) % size);
+
+        // Account for decrementing x to make max inclusive.
+        if (max == type(uint256).max && x != 0) result++;
+
+        emit log_named_uint('Bound Result', result);
     }
 }
