5a946c77e78fefc5eda56e7deefd13138096c9c5
benesjan <janbenes1234@gmail.com>
2022-05-17 12:29:26 -0500

    refactor: improved code structure, bug fixes, optimized gas consumption

--- src/bridges/compound/CompoundBridge.sol
+++ src/bridges/compound/CompoundBridge.sol
@@ -4,7 +4,6 @@ pragma solidity >=0.8.0 <=0.8.10;
 pragma abicoder v2;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
 import {IDefiBridge} from "../../interfaces/IDefiBridge.sol";
 import {AztecTypes} from "../../aztec/AztecTypes.sol";
 import "../../interfaces/IRollupProcessor.sol";
@@ -53,6 +52,9 @@ contract CompoundBridge is IDefiBridge {
     address public immutable rollupProcessor;
 
     error InvalidCaller();
+    error IncorrectInputAsset();
+    error IncorrectOutputAsset();
+    error IncorrectAuxData();
     error AsyncModeDisabled();
 
     constructor(address _rollupProcessor) public {
@@ -77,69 +79,52 @@ contract CompoundBridge is IDefiBridge {
         override
         returns (
             uint256 outputValueA,
-            uint256 outputValueB,
+            uint256,
             bool
         )
     {
         if (msg.sender != rollupProcessor) revert InvalidCaller();
 
-        if (inputAssetA.assetType == AztecTypes.AztecAssetType.ETH) {
-            // mint cETH case
-            require(
-                outputAssetA.assetType == AztecTypes.AztecAssetType.ERC20,
-                "CompoundBridge: INVALID_OUTPUT_ASSET_TYPE"
-            );
-            ICETH cToken = ICETH(outputAssetA.erc20Address);
-            cToken.mint{value: msg.value}();
-            outputValueA = cToken.balanceOf(address(this));
-            cToken.approve(rollupProcessor, outputValueA);
-        } else if (outputAssetA.assetType == AztecTypes.AztecAssetType.ETH) {
-            // redeem cETH case
-            require(
-                inputAssetA.assetType == AztecTypes.AztecAssetType.ERC20,
-                "CompoundBridge: INVALID_INPUT_ASSET_TYPE"
-            );
-            ICETH cToken = ICETH(inputAssetA.erc20Address);
-            cToken.redeem(totalInputValue);
-            outputValueA = address(this).balance;
-            IRollupProcessor(rollupProcessor).receiveEthFromBridge{value: outputValueA}(interactionNonce);
-        } else {
-            // mint or redeem cToken cases (auxData: 0 = mint, 1 = redeem)
-            require(
-                inputAssetA.assetType == AztecTypes.AztecAssetType.ERC20,
-                "CompoundBridge: INVALID_INPUT_ASSET_TYPE"
-            );
-            require(
-                outputAssetA.assetType == AztecTypes.AztecAssetType.ERC20,
-                "CompoundBridge: INVALID_OUTPUT_ASSET_TYPE"
-            );
-            if (auxData == 0) {
-                // mint
-                ICERC20 cToken = ICERC20(outputAssetA.erc20Address);
-                IERC20 underlying = IERC20(inputAssetA.erc20Address);
-                require(
-                    underlying.balanceOf(address(this)) >= totalInputValue,
-                    "CompoundBridge: INSUFFICIENT_TOKEN_BALANCE"
-                );
-                underlying.approve(address(cToken), totalInputValue);
-                cToken.mint(totalInputValue);
-                outputValueB = cToken.balanceOf(address(this));
-                cToken.approve(rollupProcessor, outputValueB);
-                cToken.transfer(rollupProcessor, outputValueB);
-            } else if (auxData == 1) {
-                // redeem
-                ICERC20 cToken = ICERC20(inputAssetA.erc20Address);
-                IERC20 underlying = IERC20(outputAssetA.erc20Address);
-                outputValueB = underlying.balanceOf(address(this));
-                require(
-                    cToken.balanceOf(address(this)) >= totalInputValue,
-                    "CompoundBridge: INSUFFICIENT_CTOKEN_BALANCE"
-                );
+        if (auxData == 0) {
+            // mint
+            if (outputAssetA.assetType != AztecTypes.AztecAssetType.ERC20) revert IncorrectOutputAsset();
+
+            if (inputAssetA.assetType == AztecTypes.AztecAssetType.ETH) {
+                ICETH cToken = ICETH(outputAssetA.erc20Address);
+                cToken.mint{value: msg.value}();
+                outputValueA = cToken.balanceOf(address(this));
+                cToken.approve(rollupProcessor, outputValueA);
+            } else if (inputAssetA.assetType == AztecTypes.AztecAssetType.ERC20) {
+                IERC20 tokenIn = IERC20(inputAssetA.erc20Address);
+                ICERC20 tokenOut = ICERC20(outputAssetA.erc20Address);
+                tokenIn.approve(address(tokenOut), totalInputValue);
+                tokenOut.mint(totalInputValue);
+                outputValueA = tokenOut.balanceOf(address(this));
+                tokenOut.approve(rollupProcessor, outputValueA);
+            } else {
+                revert IncorrectInputAsset();
+            }
+        } else if (auxData == 1) {
+            // redeem
+            if (inputAssetA.assetType != AztecTypes.AztecAssetType.ERC20) revert IncorrectInputAsset();
+
+            if (outputAssetA.assetType == AztecTypes.AztecAssetType.ETH) {
+                // redeem cETH case
+                ICETH cToken = ICETH(inputAssetA.erc20Address);
                 cToken.redeem(totalInputValue);
-                outputValueB = underlying.balanceOf(address(this)) - outputValueB;
-                underlying.approve(rollupProcessor, outputValueB);
-                underlying.transfer(rollupProcessor, outputValueB);
-            } else revert("CompoundBridge: UNRECOGNIZED_ACTION_REQUESTED");
+                outputValueA = address(this).balance;
+                IRollupProcessor(rollupProcessor).receiveEthFromBridge{value: outputValueA}(interactionNonce);
+            } else if (outputAssetA.assetType == AztecTypes.AztecAssetType.ERC20) {
+                ICERC20 tokenIn = ICERC20(inputAssetA.erc20Address);
+                IERC20 tokenOut = IERC20(outputAssetA.erc20Address);
+                tokenIn.redeem(totalInputValue);
+                outputValueA = tokenOut.balanceOf(address(this));
+                tokenOut.approve(rollupProcessor, outputValueA);
+            } else {
+                revert IncorrectOutputAsset();
+            }
+        } else {
+            revert IncorrectAuxData();
         }
     }
 
