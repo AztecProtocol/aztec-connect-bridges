e6f417c3060b3141660f485cf2e8db81ea9ade6b
benesjan <janbenes1234@gmail.com>
2022-05-25 20:19:03 +0200

    fix: hardhat configuration

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,14 @@
-import { HardhatUserConfig } from 'hardhat/config';
+import { HardhatUserConfig, subtask } from 'hardhat/config';
 import '@typechain/hardhat';
+
+const {TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS} = require("hardhat/builtin-tasks/task-names")
+
+subtask(TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS)
+  .setAction(async (_, __, runSuper) => {
+    const paths = await runSuper();
+
+    return paths.filter((p: any) => !p.includes("src/test/"));
+  });
 
 const config: HardhatUserConfig = {
   solidity: {
