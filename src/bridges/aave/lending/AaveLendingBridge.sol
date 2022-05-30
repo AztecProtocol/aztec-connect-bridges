7a7222f70f7e064f6cbe930326aa2803194ad494
LHerskind <lasse.herskind@gmail.com>
2022-05-30 17:08:26 +0100

    fix: Remove unused safemath

--- src/bridges/aave/lending/AaveLendingBridge.sol
+++ src/bridges/aave/lending/AaveLendingBridge.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import {SafeMath} from '@openzeppelin/contracts/utils/math/SafeMath.sol';
 import {SafeERC20} from '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import {IERC20Metadata} from '@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol';
@@ -32,7 +31,6 @@ import {AccountingToken} from './../AccountingToken.sol';
  * @author Lasse Herskind
  */
 contract AaveLendingBridge is IAaveLendingBridge, IDefiBridge {
-    using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
     event UnderlyingAssetListed(address underlyingAsset, address zkAToken);
