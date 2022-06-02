b9614609614ffb586954ca19c8835316e847fa33
Tajobin <tajobin@protonmail.com>
2022-06-02 23:56:28 +0200

    swapping on univ3 if cheaper

--- src/bridges/indexcoop/IndexBridge.sol
+++ src/bridges/indexcoop/IndexBridge.sol
@@ -10,8 +10,53 @@ import { AztecTypes } from "../../aztec/AztecTypes.sol";
 import { ISetToken } from "./interfaces/ISetToken.sol";
 
 
+import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
+import {IQuoter} from '@uniswap/v3-periphery/contracts/interfaces/IQuoter.sol';
+
+import "../../../lib/forge-std/src/Test.sol";
+
+interface ICurvePool {
+    function coins(uint256) external view returns (address);
+
+    function exchange(
+        int128 i,
+        int128 j,
+        uint256 dx,
+        uint256 min_dy
+    ) external;
+
+    function exchange_underlying(
+        int128 i,
+        int128 j,
+        uint256 dx,
+        uint256 min_dy
+    ) external;
+
+    function get_dy_underlying(
+        int128 i,
+        int128 j,
+        uint256 dx
+    ) external view returns (uint256);
+
+    function get_dy(
+        int128 i,
+        int128 j,
+        uint256 dx
+    ) external view returns (uint256);
+}
+
 interface RollupLike {
     function receiveEthFromBridge(uint256) external payable;
+}
+
+interface IWETH{
+    function balanceOf(address) external view returns (uint);
+
+    function deposit() external payable;
+
+    function withdraw(uint wad) external;
+
+    function transfer(address dst, uint wad) external returns (bool);
 }
 
 interface ExIssueLike {
@@ -78,16 +123,22 @@ interface ExIssueLike {
         external 
     ;
 }
-    contract IndexBridgeContract is IDefiBridge {
-        using SafeMath for uint256;
 
-        address immutable rollupProcessor;
-        address immutable exIssue = 0xB7cc88A13586D862B97a677990de14A122b74598; 
-        address immutable curve = 0xDC24316b9AE028F1497c275EB9192a3Ea0f67022;
-        address immutable weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-        address immutable steth = 0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84;
-        address immutable eth = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-        address immutable icETH = 0x7C07F7aBe10CE8e33DC6C5aD68FE033085256A84;
+contract IndexBridgeContract is IDefiBridge {
+    using SafeMath for uint256;
+
+    address immutable rollupProcessor;
+    address immutable exIssue = 0xB7cc88A13586D862B97a677990de14A122b74598; 
+    address immutable curve = 0xDC24316b9AE028F1497c275EB9192a3Ea0f67022;
+    address immutable weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address immutable steth = 0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84;
+    address immutable eth = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    address immutable icETH = 0x7C07F7aBe10CE8e33DC6C5aD68FE033085256A84;
+
+
+    address immutable univ3Router = 0xE592427A0AEce92De3Edee1F18E0157C05861564;     
+    address immutable univ3Quoter = 0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6;
+    uint24 uniFee = 3000; 
 
     constructor(address _rollupProcessor) public {
         rollupProcessor = _rollupProcessor;
@@ -113,48 +164,60 @@ interface ExIssueLike {
             uint256 outputValueB,
             bool isAsync
         )
-  {
+    {
     
-    isAsync = false;
-    // // ### INITIALIZATION AND SANITY CHECKS
-    require(msg.sender == rollupProcessor, "ExampleBridge: INVALID_CALLER");
+        isAsync = false;
+        // // ### INITIALIZATION AND SANITY CHECKS
+        require(msg.sender == rollupProcessor, "ExampleBridge: INVALID_CALLER");
 
-    if (inputAssetA.assetType == AztecTypes.AztecAssetType.ETH){
+        if (inputAssetA.assetType == AztecTypes.AztecAssetType.ETH &&
+            outputAssetA.erc20Address == address(icETH) &&
+            outputAssetB.assetType == AztecTypes.AztecAssetType.ETH 
+        ){
 
-        {
+            (outputValueA, outputValueB) = getIcEth(totalInputValue, auxData, interactionNonce); 
 
-        uint24[] memory fee;
-        address[] memory pathToSt = new address[](2); 
-        pathToSt[0] = eth;
-        pathToSt[1] = steth;
-        ExIssueLike.SwapData memory  issueData; 
+        } 
+        else if (inputAssetA.erc20Address == address(icETH) &&
+            outputAssetA.assetType == AztecTypes.AztecAssetType.ETH  
+        ){ 
 
-        issueData = ExIssueLike.SwapData(
-            pathToSt, 
-            fee,
-            curve, 
-            ExIssueLike.Exchange.Curve
-        );
-
-        // Treating auxData as max price for icETH/eth in wad 
-        uint256 setAmount = totalInputValue*auxData/1e18;
-        ExIssueLike(exIssue).issueExactSetFromETH{value: totalInputValue}(
-            ISetToken(icETH),
-            setAmount,
-            issueData,
-            issueData 
-        );
-
-        outputValueA = ISetToken(icETH).balanceOf(address(this));
-        outputValueB = address(this).balance;
-        ISetToken(icETH).approve(rollupProcessor, outputValueA);
-        RollupLike(rollupProcessor).receiveEthFromBridge{value: outputValueB}(interactionNonce);
+            outputValueA = getEth(totalInputValue, auxData, interactionNonce);
 
         }
 
-    } else if (inputAssetA.erc20Address == address(icETH)){ 
+  }
 
-       { 
+    function finalise(
+        AztecTypes.AztecAsset calldata inputAssetA,
+        AztecTypes.AztecAsset calldata inputAssetB,
+        AztecTypes.AztecAsset calldata outputAssetA,
+        AztecTypes.AztecAsset calldata outputAssetB,
+        uint256 interactionNonce,
+        uint64 auxData
+    ) external payable override returns (uint256, uint256, bool) {
+        require(false);
+    }
+
+
+    function getEth(uint256 totalInputValue, uint64 auxData, uint256 interactionNonce) internal 
+        returns (uint256 outputValueA) 
+    {
+
+        uint256 minAmountOut;
+
+        {
+        uint256 price = ICurvePool(curve).get_dy(1, 0, 1 ether);
+
+        ExIssueLike.LeveragedTokenData memory data  = 
+            ExIssueLike(exIssue).getLeveragedTokenData(ISetToken(icETH), 1 ether, true);
+
+        uint256 icNAV = (data.collateralAmount*price/1e18) - data.debtAmount;
+
+        minAmountOut = totalInputValue*(auxData*icNAV/1e18)/1e18;
+
+        }
+
         uint24[] memory fee;
         address[] memory pathToEth = new address[](2); 
         pathToEth[0] = steth;
@@ -168,36 +231,141 @@ interface ExIssueLike {
             ExIssueLike.Exchange.Curve
         );
 
-        //Treat auxdata as eth/iceth wad
+        bytes memory path = abi.encodePacked(address(icETH), uniFee);
+        path = abi.encodePacked(path, weth);
+        uint256 ethFromDex = IQuoter(univ3Quoter).quoteExactInput(path, totalInputValue);
 
-        uint256 minOutput = auxData*totalInputValue/1e18;
-        ISetToken(icETH).approve(address(exIssue), totalInputValue);
-
-        ExIssueLike(exIssue).redeemExactSetForETH(
-            ISetToken(icETH),
-            totalInputValue,
-            minOutput,
-            redeemData,
+        uint256 ethFromEx  = ExIssueLike(exIssue).getRedeemExactSet(
+            ISetToken(icETH), 
+            totalInputValue, 
+            redeemData, 
             redeemData
         );
 
-        outputValueA = address(this).balance;
-        RollupLike(rollupProcessor).receiveEthFromBridge{value: outputValueA}(interactionNonce);
-       }
+        if (ethFromEx > ethFromDex) {
+            console2.log("Redeem Cheaper");
+            //Treat auxdata as eth/iceth wad
+            uint256 minOutput = auxData*totalInputValue/1e18;
+            ISetToken(icETH).approve(address(exIssue), totalInputValue);
+
+            ExIssueLike(exIssue).redeemExactSetForETH(
+                ISetToken(icETH),
+                totalInputValue,
+                minAmountOut,
+                redeemData,
+                redeemData
+            );
+
+            outputValueA = address(this).balance;
+            RollupLike(rollupProcessor).receiveEthFromBridge{value: outputValueA}(interactionNonce);
+        } else {
+
+            console2.log("Buying Cheaper");
+    
+            IERC20(icETH).approve(univ3Router, totalInputValue);
+            ISwapRouter.ExactInputSingleParams memory params =
+                ISwapRouter.ExactInputSingleParams({
+                    tokenIn: icETH,
+                    tokenOut: weth,
+                    fee: uniFee,
+                    recipient: address(this),
+                    deadline: block.timestamp,
+                    amountIn: totalInputValue,
+                    amountOutMinimum: minAmountOut ,               //NOT SAFE !! #ch
+                    sqrtPriceLimitX96: 0
+            });
+            outputValueA = ISwapRouter(univ3Router).exactInputSingle(params);
+            uint256 wethBalance = IWETH(weth).balanceOf(address(this));
+
+            IWETH(weth).withdraw(outputValueA);
+            RollupLike(rollupProcessor).receiveEthFromBridge{value: outputValueA}(interactionNonce);
+        }
+        
+
+
+    }
+    
+    function getIcEth(uint256 totalInputValue, uint64 auxData, uint256 interactionNonce) 
+        internal 
+        returns (uint256 outputValueA, uint256 outputValueB) 
+    {
+
+
+        uint256 minAmountOut;
+
+        {
+        uint256 price = ICurvePool(curve).get_dy(1, 0, 1 ether);
+
+        ExIssueLike.LeveragedTokenData memory data  = 
+            ExIssueLike(exIssue).getLeveragedTokenData(ISetToken(icETH), 1 ether, true);
+
+        uint256 icNAV = (data.collateralAmount*price/1e18) - data.debtAmount; // eth/icETH
+
+        minAmountOut = auxData*totalInputValue/icNAV;
+
+        }
+
+        //Set up Exissue data
+        uint24[] memory fee;
+        address[] memory pathToSt = new address[](2); 
+        pathToSt[0] = eth;
+        pathToSt[1] = steth;
+        ExIssueLike.SwapData memory issueData = ExIssueLike.SwapData(
+            pathToSt, 
+            fee,
+            curve, 
+            ExIssueLike.Exchange.Curve
+        );
+
+        //Check if uni is cheaper than Set Issue
+        bytes memory path = abi.encodePacked(weth, uniFee);
+        path = abi.encodePacked(path, address(icETH));
+        uint256 tokensFromDex = IQuoter(univ3Quoter).quoteExactInput(path, totalInputValue);
+
+        uint256 issueCostOfTokens = ExIssueLike(exIssue).getIssueExactSet(
+            ISetToken(icETH),
+            tokensFromDex, 
+            issueData, 
+            issueData
+        ); 
+
+        // Cheaper to issue
+        if (issueCostOfTokens < totalInputValue){
+            console2.log("Issuing Cheaper");
+
+            ExIssueLike(exIssue).issueExactSetFromETH{value: totalInputValue}(
+                ISetToken(icETH),
+                minAmountOut,
+                issueData,
+                issueData 
+            );
+
+            outputValueA = ISetToken(icETH).balanceOf(address(this));
+            outputValueB = address(this).balance;
+            ISetToken(icETH).approve(rollupProcessor, outputValueA);
+            RollupLike(rollupProcessor).receiveEthFromBridge{value: outputValueB}(interactionNonce);
+
+        } else { // Cheaper to buy
+            console2.log("Buying Cheaper");
+    
+            IWETH(weth).deposit{value: totalInputValue}();
+            IERC20(weth).approve(univ3Router, totalInputValue);
+            ISwapRouter.ExactInputSingleParams memory params =
+                ISwapRouter.ExactInputSingleParams({
+                    tokenIn: weth,
+                    tokenOut: icETH,
+                    fee: uniFee,
+                    recipient: address(this),
+                    deadline: block.timestamp,
+                    amountIn: totalInputValue,
+                    amountOutMinimum: minAmountOut ,              
+                    sqrtPriceLimitX96: 0
+            });
+
+            outputValueA = ISwapRouter(univ3Router).exactInputSingle(params);
+            ISetToken(icETH).approve(rollupProcessor, outputValueA);
+        }
 
     }
 
-  }
-
-
-    function finalise(
-        AztecTypes.AztecAsset calldata inputAssetA,
-        AztecTypes.AztecAsset calldata inputAssetB,
-        AztecTypes.AztecAsset calldata outputAssetA,
-        AztecTypes.AztecAsset calldata outputAssetB,
-        uint256 interactionNonce,
-        uint64 auxData
-    ) external payable override returns (uint256, uint256, bool) {
-        require(false);
-    }
 }
--- src/test/indexcoop/Index.t.sol
+++ src/test/indexcoop/Index.t.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.10;
-
+pragma experimental ABIEncoderV2;
 import {DefiBridgeProxy} from "./../../aztec/DefiBridgeProxy.sol";
 import {RollupProcessor} from "./../../aztec/RollupProcessor.sol";
 
@@ -11,6 +11,104 @@ import {AztecTypes} from "./../../aztec/AztecTypes.sol";
 import "../../../lib/forge-std/src/Test.sol";
 import { ISetToken } from "./../../bridges/indexcoop/interfaces//ISetToken.sol";
 
+import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
+import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
+
+import {IQuoter} from '@uniswap/v3-periphery/contracts/interfaces/IQuoter.sol';
+
+
+interface ICurvePool {
+    function coins(uint256) external view returns (address);
+
+    function exchange(
+        int128 i,
+        int128 j,
+        uint256 dx,
+        uint256 min_dy
+    ) external;
+
+    function exchange_underlying(
+        int128 i,
+        int128 j,
+        uint256 dx,
+        uint256 min_dy
+    ) external;
+
+    function get_dy_underlying(
+        int128 i,
+        int128 j,
+        uint256 dx
+    ) external view returns (uint256);
+
+    function get_dy(
+        int128 i,
+        int128 j,
+        uint256 dx
+    ) external view returns (uint256);
+}
+
+
+interface DEXAdapter{
+
+    enum Exchange { None, Quickswap, Sushiswap, UniV3, Curve }
+
+    /* ============ Structs ============ */
+    struct Addresses {
+        address quickRouter;
+        address sushiRouter;
+        address uniV3Router;
+        address uniV3Quoter;
+        address curveAddressProvider;
+        address curveCalculator;
+        // Wrapped native token (WMATIC on polygon)
+        address weth;
+    }
+
+    struct SwapData {
+        address[] path;
+        uint24[] fees;
+        address pool;
+        Exchange exchange;
+    }
+
+    function swapExactTokensForTokens(
+        Addresses memory _addresses,
+        uint256 _amountIn,
+        uint256 _minAmountOut,
+        SwapData memory _swapData
+    )
+        external
+        returns (uint256);
+
+    function getAmountOut(
+        Addresses memory,
+        SwapData memory,
+        uint256 
+    )
+        external
+        returns (uint256);
+    /*
+    function swapExactTokensForTokens(
+        address,
+        address,
+        address,
+        address,
+        address,
+        address,
+        address,
+        HLuint256,
+        uint256,
+        address[] memory,
+        uint24[] memory,
+        address,
+        uint8
+    )
+        external
+        returns (uint256);
+    */
+
+}
+
 interface ExIssueLike {
 
     enum Exchange { None, Quickswap, Sushiswap, UniV3, Curve }
@@ -20,6 +118,9 @@ interface ExIssueLike {
         address pool;
         Exchange exchange;
     }
+
+
+    function addresses() external returns(address,address,address,address,address,address,address);
 
     struct LeveragedTokenData {
         address collateralAToken;
@@ -88,6 +189,11 @@ contract IndexTest is Test {
     address ethAddress = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
     ISetToken icETH = ISetToken(0x7C07F7aBe10CE8e33DC6C5aD68FE033085256A84);
 
+    address adapterAddress = 0x25a8803B9B611cc13D807829D73049cd803f6FCa;
+    DEXAdapter adapter = DEXAdapter(adapterAddress);
+
+    //ISwapRouter public immutable swapRouter;
+
     function _aztecPreSetup() internal {
         defiBridgeProxy = new DefiBridgeProxy();
         rollupProcessor = new RollupProcessor(address(defiBridgeProxy));
@@ -103,13 +209,96 @@ contract IndexTest is Test {
 
     }
 
+
     function testIssueSet() public {
+
+        uint256 stEthPrice = ICurvePool(curvePoolAddress).get_dy(1, 0, 1 ether);
+
+        ExIssueLike.LeveragedTokenData memory data  = 
+            ExIssue.getLeveragedTokenData(icETH, 1 ether, true);
+
+        uint256 icEthValue = (data.collateralAmount*stEthPrice/1e18) - data.debtAmount;
+
         uint256 inputValue = 10 ether;
-        uint256 maxPrice = 0.9 ether; // icETH/eth in wad 
+        uint256 maxSlip = 0.99 ether; 
+        (uint256 newicETH, uint256 refundEth) = issueSet(inputValue, maxSlip);
+        console2.log('New icETH', newicETH);
+        console2.log('Refunded ETH', refundEth);
+
+        assertLe(maxSlip*(inputValue-refundEth)/icEthValue, newicETH);
+        
+        console2.log('ETH to icETH ratio', 1000*(inputValue-refundEth)/newicETH);
+    }
+
+
+    function testRedeem() public{
+        uint256 stEthPrice = ICurvePool(curvePoolAddress).get_dy(1, 0, 1 ether);
+
+        ExIssueLike.LeveragedTokenData memory data  = 
+            ExIssue.getLeveragedTokenData(icETH, 1 ether, true);
+
+        uint256 icEthValue = (data.collateralAmount*stEthPrice/1e18) - data.debtAmount;
+
+        address hoaxAddress = 0xA400f843f0E577716493a3B0b8bC654C6EE8a8A3;
+        hoax(hoaxAddress, 10);
+
+        uint256 inputValue = 10 ether;
+        uint256 maxSlip = 0.995 ether;
+        
+        icETH.transfer(address(rollupProcessor), inputValue);
+        uint256 newEth = redeemSet(inputValue, maxSlip);  
+        
+        console2.log('New ETH', newEth);
+        console2.log('ETH to icETH ratio', 1000*newEth/inputValue);
+        assertLe(inputValue*(icEthValue*maxSlip/1e18)/1e18, newEth);
+
+   } 
+
+
+
+
+
+
+
+    function redeemSet(uint256 inputValue, uint256 maxPrice) internal returns (uint256) {
+        AztecTypes.AztecAsset memory empty;
+
+        AztecTypes.AztecAsset memory ethAztecAsset = AztecTypes.AztecAsset({
+            id: 1,
+            erc20Address: ethAddress ,
+            assetType: AztecTypes.AztecAssetType.ETH
+        });
+
+        AztecTypes.AztecAsset memory icethAztecAssetA = AztecTypes.AztecAsset({
+            id: 2,
+            erc20Address: address(icETH),
+            assetType: AztecTypes.AztecAssetType.ERC20
+        });
+
+        uint256 _maxPrice = maxPrice;
+        uint256 _inputValue = inputValue;
+
+        ( uint256 outputValueA, uint256 outputValueB, bool isAsync) =
+             rollupProcessor.convert(
+                address(indexBridge),
+                icethAztecAssetA,
+                empty,
+                ethAztecAsset,
+                empty,
+                _inputValue,
+                2,
+                _maxPrice
+        );
+
+        return address(rollupProcessor).balance;
+
+    }
+
+
+    function issueSet(uint256 inputValue, uint256 maxPrice) internal returns (uint256, uint256){
         deal(address(rollupProcessor), inputValue);
 
         AztecTypes.AztecAsset memory empty;
-
         AztecTypes.AztecAsset memory ethAztecAsset = AztecTypes.AztecAsset({
             id: 1,
             erc20Address: ethAddress ,
@@ -122,6 +311,9 @@ contract IndexTest is Test {
             assetType: AztecTypes.AztecAssetType.ERC20
         });
 
+        uint256 _maxPrice = maxPrice;
+        uint256 _inputValue = inputValue;
+
         ( uint256 outputValueA, uint256 outputValueB, bool isAsync) =
              rollupProcessor.convert(
                 address(indexBridge),
@@ -129,54 +321,13 @@ contract IndexTest is Test {
                 empty,
                 icethAztecAssetA,
                 ethAztecAsset,
-                inputValue,
+                _inputValue,
                 1,
-                maxPrice
+                _maxPrice
         );
 
-        uint256 newicETH = icETH.balanceOf(address(rollupProcessor));
-        assertEq(inputValue*maxPrice/1e18, newicETH);
-
+        return (icETH.balanceOf(address(rollupProcessor)), address(rollupProcessor).balance);
     }
-
-
-    function testRedeemSet() public{
-        uint256 inputValue = 0.9 ether;
-        uint256 maxPrice = 0.9 ether; // eth/icETH max price in wad
-        deal(address(icETH), address(rollupProcessor), inputValue);
-
-        AztecTypes.AztecAsset memory empty;
-
-        AztecTypes.AztecAsset memory ethAztecAsset = AztecTypes.AztecAsset({
-            id: 1,
-            erc20Address: ethAddress ,
-            assetType: AztecTypes.AztecAssetType.ETH
-        });
-
-        AztecTypes.AztecAsset memory icethAztecAssetA = AztecTypes.AztecAsset({
-            id: 2,
-            erc20Address: address(icETH),
-            assetType: AztecTypes.AztecAssetType.ERC20
-        });
-
-
-        ( uint256 outputValueA, uint256 outputValueB, bool isAsync) =
-             rollupProcessor.convert(
-                address(indexBridge),
-                icethAztecAssetA,
-                empty,
-                ethAztecAsset,
-                empty,
-                inputValue,
-                1,
-                maxPrice
-        );
-
-        uint256 newEth = address(rollupProcessor).balance;
-        assertGt(newEth, inputValue*maxPrice/1e18);
-    }
-
-
 
 
 
@@ -185,6 +336,92 @@ contract IndexTest is Test {
 
 
     /*
+
+    function testGetCurvePrice() public{
+        uint256 price = ICurvePool(curvePoolAddress).get_dy(1, 0, 1 ether);
+        console2.log('Price of stETH/ETH', price);
+
+       ExIssueLike.LeveragedTokenData memory data  = 
+            ExIssue.getLeveragedTokenData(icETH, 1 ether, true);
+
+
+        uint256 icNAV = (data.collateralAmount*price/1e18) - data.debtAmount;
+
+        console2.log('Collateral Amount', data.collateralAmount);
+        console2.log('Det Amount', data.debtAmount);
+
+        console2.log('NAV of 1 icETH', icNAV);
+
+    }
+    function testPriceCompare() public{
+        uint256 inputAmount = 10 ether;
+
+        //deal(wethAddress, address(this), inputAmount);
+
+        //address router = 0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45;
+        address router = 0xE592427A0AEce92De3Edee1F18E0157C05861564;     
+
+
+        uint24 feee = 3000; 
+        address _quoter = 0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6;
+
+        bytes memory path = abi.encodePacked(wethAddress, feee);
+        path = abi.encodePacked(path, address(icETH));
+        uint256 quote = IQuoter(_quoter).quoteExactInput(path, inputAmount);
+        console2.log('Quoted', quote);
+
+        uint24[] memory fee;
+        address[] memory pathToSt = new address[](2); 
+        pathToSt[0] = ethAddress;
+        pathToSt[1] = stethAddress;
+        ExIssueLike.SwapData memory  issueData; 
+
+        issueData = ExIssueLike.SwapData(
+            pathToSt, 
+            fee,
+            curvePoolAddress, 
+            ExIssueLike.Exchange.Curve
+        );
+
+
+        uint256 cost = ExIssue.getIssueExactSet(icETH, quote, issueData, issueData); 
+        console2.log('Issue Sets', cost);
+    }
+
+    function testBuySet() public{
+        uint256 inputAmount = 1 ether;
+
+        deal(wethAddress, address(this), inputAmount);
+
+        //address router = 0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45;
+        address router = 0xE592427A0AEce92De3Edee1F18E0157C05861564;  
+
+        uint24 fee = 3000; 
+        address _quoter = 0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6;  
+        bytes memory path = abi.encodePacked(wethAddress, fee);
+        path = abi.encodePacked(path, address(icETH));
+        uint256 quote = IQuoter(_quoter).quoteExactInput(path, inputAmount);
+        console2.log('Quoted', quote);
+
+        //approve router  on first token
+        IERC20(wethAddress).approve(router, inputAmount);
+        // ISwapRouter.ExactInputSingleParams memory params = 
+
+        ISwapRouter.ExactInputSingleParams memory params =
+            ISwapRouter.ExactInputSingleParams({
+                tokenIn: wethAddress,
+                tokenOut: address(icETH),
+                fee: fee,
+                recipient: address(this),
+                deadline: block.timestamp,
+                amountIn: inputAmount,
+                amountOutMinimum: 0.000000008 ether,
+                sqrtPriceLimitX96: 0
+        });
+
+        uint amount = ISwapRouter(router).exactInputSingle(params);
+        console.log('Amount out', amount);
+    }
 
     function testTokenData() public {
        ExIssueLike.LeveragedTokenData memory data  = 
