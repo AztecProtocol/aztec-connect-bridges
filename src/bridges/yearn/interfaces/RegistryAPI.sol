a4dc3e6616e494d6a92d01858a03c135973c0140
johhonn <jjj.may377@gmail.com>
2022-06-07 11:51:32 -0400

    remove extraneous code

--- src/bridges/yearn/interfaces/RegistryAPI.sol
+++
@@ -1,9 +0,0 @@
-interface RegistryAPI {
-    function governance() external view returns (address);
-
-    function latestVault(address token) external view returns (address);
-
-    function numVaults(address token) external view returns (uint256);
-
-    function vaults(address token, uint256 deploymentId) external view returns (address);
-}
