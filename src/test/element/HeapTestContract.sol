59752a8bba29de883127254dac30a54bff7dba83
benesjan <janbenes1234@gmail.com>
2022-06-10 11:44:48 +0200

    fix: linting errors in HeapTestContract.sol

--- src/test/element/HeapTestContract.sol
+++ src/test/element/HeapTestContract.sol
@@ -1,33 +1,32 @@
-pragma solidity 0.8.10;
+// SPDX-License-Identifier: GPL-2.0-only
+// Copyright 2022 Spilsbury Holdings Ltd
+pragma solidity >=0.8.4;
 
 import {MinHeap} from "../../bridges/element/MinHeap.sol";
-import "../../../lib/ds-test/src/test.sol";
-
-import {console} from "forge-std/console.sol";
 
 contract HeapTestContract {
     using MinHeap for MinHeap.MinHeapData;
 
-    MinHeap.MinHeapData heap;
+    MinHeap.MinHeapData private heap;
 
-    constructor(uint32 initialSize) {
-        heap.initialise(initialSize);
+    constructor(uint32 _initialSize) {
+        heap.initialise(_initialSize);
     }
 
-    function add(uint64 value) public {
-        heap.add(value);
+    function add(uint64 _value) public {
+        heap.add(_value);
     }
 
-    function min() public view returns (uint64) {
-        return heap.min();
-    }
-
-    function remove(uint64 value) public {
-        heap.remove(value);
+    function remove(uint64 _value) public {
+        heap.remove(_value);
     }
 
     function pop() public {
         heap.pop();
+    }
+
+    function min() public view returns (uint64) {
+        return heap.min();
     }
 
     function size() public view returns (uint256) {
