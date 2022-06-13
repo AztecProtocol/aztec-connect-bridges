901b2b79ead5d6ffe804e6323e982454e92ff5b6
LHerskind <lasse.herskind@gmail.com>
2022-06-13 11:32:55 +0100

    fix: IWETH rebase

--- src/interfaces/IWETH.sol
+++ src/interfaces/IWETH.sol
@@ -1,11 +1,15 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity >=0.7.5;
-pragma abicoder v2;
+// SPDX-License-Identifier: GPL-2.0-only
+// Copyright 2022 Spilsbury Holdings Ltd
+pragma solidity >=0.8.4;
 
-import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
-
-interface WETH is IERC20 {
+interface IWETH {
     function deposit() external payable;
 
-    function withdraw(uint256 wad) external;
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function transfer(address to, uint256 amount) external returns (bool);
+
+    function balanceOf(address user) external view returns (uint256);
+
+    function withdraw(uint256 amount) external;
 }
