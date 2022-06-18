113c9975e5508cf7ff9b342fb0377b910c6f9ce6
LHerskind <lasse.herskind@gmail.com>
2022-06-18 08:14:43 +0000

    Update Lido test

--- src/interfaces/lido/ILido.sol
+++ src/interfaces/lido/ILido.sol
@@ -4,5 +4,11 @@ pragma solidity >=0.8.4;
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface ILido is IERC20 {
+    function sharesOf(address _account) external view returns (uint256);
+
+    function getSharesByPooledEth(uint256 _amount) external view returns (uint256);
+
+    function getPooledEthByShares(uint256 _amount) external view returns (uint256);
+
     function submit(address _referral) external payable returns (uint256);
 }
