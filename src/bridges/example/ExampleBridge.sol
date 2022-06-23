b414b5f662daa6a7cd5b648134933f07b594702f
benesjan <janbenes1234@gmail.com>
2022-06-23 10:10:11 +0100

    refactor: explicitly specifying output asset + improved comments

--- src/bridges/example/ExampleBridge.sol
+++ src/bridges/example/ExampleBridge.sol
@@ -16,21 +16,22 @@ import {BridgeBase} from "../base/BridgeBase.sol";
  */
 contract ExampleBridgeContract is BridgeBase {
     /**
-     * @notice Set address of RollupProcessor.sol.
-     * @param _rollupProcessor Address of RollupProcessor.sol
+     * @notice Set address of rollup processor
+     * @param _rollupProcessor Address of rollup processor
      */
     constructor(address _rollupProcessor) BridgeBase(_rollupProcessor) {}
 
     /**
-     * @notice A function which returns an _inputValue amount of _inputAssetA.
-     * @param _inputAssetA - Arbitrary ERC20 token.
+     * @notice A function which returns an _inputValue amount of _inputAssetA
+     * @param _inputAssetA - Arbitrary ERC20 token
+     * @param _outputAssetA - Equal to _inputAssetA
      * @return outputValueA - the amount of output asset to return
-     * @dev In this case _outputAssetA equals _inputAssetA.
+     * @dev In this case _outputAssetA equals _inputAssetA
      */
     function convert(
         AztecTypes.AztecAsset memory _inputAssetA,
         AztecTypes.AztecAsset memory,
-        AztecTypes.AztecAsset memory,
+        AztecTypes.AztecAsset memory _outputAssetA,
         AztecTypes.AztecAsset memory,
         uint256 _inputValue,
         uint256,
@@ -49,9 +50,10 @@ contract ExampleBridgeContract is BridgeBase {
     {
         // Check the input asset is ERC20
         if (_inputAssetA.assetType != AztecTypes.AztecAssetType.ERC20) revert ErrorLib.InvalidInputA();
+        if (_outputAssetA.erc20Address != _inputAssetA.erc20Address) revert ErrorLib.InvalidOutputA();
         // Return the input value of input asset
         outputValueA = _inputValue;
         // Approve rollup processor to take input value of input asset
-        IERC20(_inputAssetA.erc20Address).approve(ROLLUP_PROCESSOR, _inputValue);
+        IERC20(_outputAssetA.erc20Address).approve(ROLLUP_PROCESSOR, _inputValue);
     }
 }
