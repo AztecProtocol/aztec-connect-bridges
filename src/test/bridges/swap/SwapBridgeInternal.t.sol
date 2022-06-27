9c3050ab15f0b750558398e3931f99236a9829ae
benesjan <janbenes1234@gmail.com>
2022-06-27 09:39:26 +0200

    test: testing decoding of both split paths

--- src/test/bridges/swap/SwapBridgeInternal.t.sol
+++ src/test/bridges/swap/SwapBridgeInternal.t.sol
@@ -7,6 +7,7 @@ import {SwapBridge} from "../../../bridges/swap/SwapBridge.sol";
 
 contract SwapBridgeInternalTest is DSTest, SwapBridge(address(0)) {
     address public constant LUSD = 0x5f98805A4E8be255a32880FDeC7F6728C6568bA0;
+    address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address public constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     address public constant LQTY = 0x6DEA81C8171D0bA574754EF6F8b412F2Ed88c54D;
     address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
@@ -14,14 +15,37 @@ contract SwapBridgeInternalTest is DSTest, SwapBridge(address(0)) {
     function setUp() public {}
 
     function testDecodePath() public {
-        //            500    3000    3000
-        // PATH1 LUSD -> DAI -> WETH -> LQTY    30% of input 0011110 01 100 10 001 10
+        bytes memory referenceSplitPath1 = abi.encodePacked(
+            LUSD,
+            uint24(500),
+            USDC,
+            uint24(3000),
+            WETH,
+            uint24(3000),
+            LQTY
+        );
+        bytes memory referenceSplitPath2 = abi.encodePacked(
+            LUSD,
+            uint24(500),
+            DAI,
+            uint24(3000),
+            WETH,
+            uint24(3000),
+            LQTY
+        );
+
         //            500     3000    3000
-        // PATH2 LUSD -> USDC -> WETH -> LQTY   70% of input 1000110 01 010 10 001 10
-        // 0000000000000000000000000000000000000000000001111111 111111111111
+        // PATH1 LUSD -> USDC -> WETH -> LQTY   70% of input 1000110 01 010 10 001 10
+        //            500    3000    3000
+        // PATH2 LUSD -> DAI -> WETH -> LQTY    30% of input 0011110 01 100 10 001 10
         // 00000000000000000000000000 | 0011110 01 100 10 001 10 | 1000110 01 010 10 001 10
         uint64 encodedPath = 0xF32346546;
         SwapBridge.Path memory path = _decodePath(LUSD, encodedPath, LQTY);
+
+        assertEq(path.percentage1, 70, "Incorrect percentage 1");
+        assertEq(string(path.splitPath1), string(referenceSplitPath1), "Split path 1 incorrectly encoded");
+        assertEq(path.percentage2, 30, "Incorrect percentage 2");
+        assertEq(string(path.splitPath2), string(referenceSplitPath2), "Split path 2 incorrectly encoded");
     }
 
     function testDecodeSplitPathAllMiddleTokensUsed() public {
