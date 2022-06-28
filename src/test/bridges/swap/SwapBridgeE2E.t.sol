c03242e46cbcf90a30fa58e9ed6f6049fa81018a
benesjan <janbenes1234@gmail.com>
2022-06-28 14:08:32 +0200

    test: WIP on E2E test

--- src/test/bridges/swap/SwapBridgeE2E.t.sol
+++ src/test/bridges/swap/SwapBridgeE2E.t.sol
@@ -8,6 +8,7 @@ import {AztecTypes} from "../../../aztec/libraries/AztecTypes.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ErrorLib} from "../../../bridges/base/ErrorLib.sol";
 import {SwapBridge} from "../../../bridges/swap/SwapBridge.sol";
+import {IQuoter} from "../../../interfaces/uniswapv3/IQuoter.sol";
 
 contract SwapBridgeE2ETest is BridgeTestBase {
     address public constant LUSD = 0x5f98805A4E8be255a32880FDeC7F6728C6568bA0;
@@ -15,6 +16,13 @@ contract SwapBridgeE2ETest is BridgeTestBase {
     address public constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     address public constant LQTY = 0x6DEA81C8171D0bA574754EF6F8b412F2Ed88c54D;
     address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+
+    IQuoter public QUOTER = IQuoter(0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6);
+
+    bytes private referenceSplitPath1 =
+    abi.encodePacked(LUSD, uint24(500), USDC, uint24(3000), WETH, uint24(3000), LQTY);
+    bytes private referenceSplitPath2 =
+    abi.encodePacked(LUSD, uint24(500), DAI, uint24(3000), WETH, uint24(3000), LQTY);
 
     SwapBridge private bridge;
 
@@ -34,17 +42,55 @@ contract SwapBridgeE2ETest is BridgeTestBase {
         vm.label(LQTY, "LQTY");
         vm.label(USDC, "USDC");
 
-        // Impersonate the multi-sig to add a new bridge
-        vm.prank(MULTI_SIG);
+        // Impersonate the multi-sig to add a new bridge and assets
+        vm.startPrank(MULTI_SIG);
 
-        // List the example-bridge with a gasLimit of 100K
-        ROLLUP_PROCESSOR.setSupportedBridge(address(bridge), 100000);
+        // List the example-bridge with a gasLimit of 1M
+        ROLLUP_PROCESSOR.setSupportedBridge(address(bridge), 1000000);
+
+        // List the assets with a gasLimit of 100k
+        ROLLUP_PROCESSOR.setSupportedAsset(LUSD, 100000);
+        ROLLUP_PROCESSOR.setSupportedAsset(LQTY, 100000);
+
+        vm.stopPrank();
 
         // Fetch the id of the example bridge
         id = ROLLUP_PROCESSOR.getSupportedBridgesLength();
     }
 
     function testSwap() public {
-        // TODO
+        uint256 swapAmount = 1e21; // 1000 LUSD
+
+        //            500     3000    3000
+        // PATH1 LUSD -> USDC -> WETH -> LQTY   70% of input 1000110 01 010 10 001 10
+        //            500    3000    3000
+        // PATH2 LUSD -> DAI -> WETH -> LQTY    30% of input 0011110 01 100 10 001 10
+        // MIN PRICE: significand 0, exponent 0
+        // @dev Setting min price to 0 here in order to avoid issues with changing mainnet liquidity
+        // 000000000000000000000 00000 | 0011110 01 100 10 001 10 | 1000110 01 010 10 001 10
+        uint64 encodedPath = 0xF32346546;
+
+        bridge.preApproveTokenPair(LUSD, LQTY);
+
+        // Define input and output assets
+        AztecTypes.AztecAsset memory lusdAsset = getRealAztecAsset(LUSD);
+        AztecTypes.AztecAsset memory lqtyAsset = getRealAztecAsset(LQTY);
+
+        deal(LUSD, address(ROLLUP_PROCESSOR), swapAmount);
+
+        // Compute quote
+        uint256 swapAmountSplitPath1 = (swapAmount * 70) / 100;
+        uint256 quote = QUOTER.quoteExactInput(referenceSplitPath1, swapAmountSplitPath1);
+        quote += QUOTER.quoteExactInput(referenceSplitPath1, swapAmount - swapAmountSplitPath1);
+
+        // Computes the encoded data for the specific bridge interaction
+        uint256 bridgeId = encodeBridgeId(id, lusdAsset, emptyAsset, lqtyAsset, emptyAsset, encodedPath);
+
+        vm.expectEmit(true, true, false, true);
+        // Second part of cheatcode, emit the event that we are to match against.
+        emit DefiBridgeProcessed(bridgeId, getNextNonce(), swapAmount, quote, 0, true, "");
+
+        // Execute the rollup with the bridge interaction. Ensure that event as seen above is emitted.
+        sendDefiRollup(bridgeId, swapAmount);
     }
 }
