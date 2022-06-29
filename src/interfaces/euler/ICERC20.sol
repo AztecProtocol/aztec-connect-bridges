60207f7739f1f052929c6b8df5cd13569cf783a0
Beno <105245519+YtzBeno@users.noreply.github.com>
2022-06-29 13:02:55 +0200

    Update ICERC20.sol

--- src/interfaces/euler/ICERC20.sol
+++ src/interfaces/euler/ICERC20.sol
@@ -1,1 +1,18 @@
+pragma solidity >=0.8.4;
 
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface ICERC20 is IERC20 {
+    //first uint = subAccountId, second uint = amount
+    
+    function deposit(uint, uint) external;          
+    function withdraw(uint, uint) external;        
+    function borrow(uint, uint) external;
+    
+    
+    
+  
+    
+    
+    
+}
