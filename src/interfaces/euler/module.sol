8fe2b31f808123506ed5f25e929afb98b397f474
Beno <105245519+YtzBeno@users.noreply.github.com>
2022-06-29 18:34:29 +0200

    Update module.sol

--- src/interfaces/euler/module.sol
+++ src/interfaces/euler/module.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.8.4;
 interface module {
 
     function enterMarket(uint, address) external; //address here = collateral
-    function underlyingToAssetConfigUnresolved(address) external view returns (IEuler.AssetConfig memory config);
+    function underlyingToAssetConfig(address) external view returns (IEuler.AssetConfig memory);
     function underlyingToEToken(address) external view returns (address);
     
     
