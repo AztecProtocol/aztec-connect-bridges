e3afee4392767a6879ecefee19ee5dab8afab4fb
Beno <105245519+YtzBeno@users.noreply.github.com>
2022-06-29 19:27:35 +0200

    Update IEERC20.sol

--- src/interfaces/euler/IEERC20.sol
+++ src/interfaces/euler/IEERC20.sol
@@ -7,18 +7,9 @@ interface IEERC20 is IERC20 {
     //First uint = subAccountID (0 for primary, 1-255 for a sub-account), Second uint = amount
 
     function deposit(uint, uint) external;
-    function withdraw(uint, uint) external;
-    function borrow(uint, uint) external;
-    function repay(uint, uint) external;
+    function withdraw(uint, uint) external; 
     function balanceOfUnderlying(address) external view returns (uint); //address here = account 
     function approve(address, uint) external returns (bool); //address = spender (Euler_Mainnet)
-
+   
     
-    
-    
-    
-    
-    
-
-
 }
