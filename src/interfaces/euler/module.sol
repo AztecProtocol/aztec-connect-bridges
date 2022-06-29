9fd601e8868db702978af5f37827ed9d5025f661
Beno <105245519+YtzBeno@users.noreply.github.com>
2022-06-29 19:30:34 +0200

    Update module.sol

--- src/interfaces/euler/module.sol
+++ src/interfaces/euler/module.sol
@@ -5,6 +5,7 @@ interface module {
     function enterMarket(uint, address) external; //address here = collateral
     function underlyingToAssetConfig(address) external view returns (IEuler.AssetConfig memory);
     function underlyingToEToken(address) external view returns (address);
+    function underlyingToDToken(address) external view returns (address);
     
     
     
