1be624bfad88d7bd33a9205ca31666246367effa
Beno <105245519+YtzBeno@users.noreply.github.com>
2022-06-30 15:42:01 +0200

    Update IEulerEToken.sol

--- src/interfaces/euler/IEulerEToken.sol
+++ src/interfaces/euler/IEulerEToken.sol
@@ -2,7 +2,7 @@ pragma solidity >=0.8.4;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-interface IEERC20 is IERC20 {
+interface IEulerEToken is IERC20 {
 
     //First uint = subAccountID (0 for primary, 1-255 for a sub-account), Second uint = amount
 
