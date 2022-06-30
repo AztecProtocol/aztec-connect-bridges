beedaed473c25f4e723901f42de47975e050dcc8
Beno <105245519+YtzBeno@users.noreply.github.com>
2022-06-30 17:18:34 +0200

    Update IEulerDToken.sol

--- src/interfaces/euler/IEulerDToken.sol
+++ src/interfaces/euler/IEulerDToken.sol
@@ -2,8 +2,6 @@ pragma solidity >=0.8.4;
 
 interface IEulerDToken {
 
-    function borrow(uint, uint) external;
-    function repay(uint, uint) external;
     function balanceOf(address) external view returns (uint);
 
     
