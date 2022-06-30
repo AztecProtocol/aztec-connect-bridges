f80b8e2c59f4d4db636ee170a4b78eb3cf85dff3
Beno <105245519+YtzBeno@users.noreply.github.com>
2022-06-30 15:37:10 +0200

    Update EulerLendingBridge.sol

--- src/bridges/euler/EulerLendingBridge.sol
+++ src/bridges/euler/EulerLendingBridge.sol
@@ -1,1 +1,26 @@
+// SPDX-License-Identifier: GPL-2.0-only
+// Copyright 2022 Spilsbury Holdings Ltd
+pragma solidity >=0.8.4;
 
+import {IRollupProcessor} from "../../aztec/interfaces/IRollupProcessor.sol";
+
+import {ErrorLib} from "./../base/ErrorLib.sol";
+import {BridgeBase} from "./../base/BridgeBase.sol";
+import {AztecTypes} from "../../aztec/libraries/AztecTypes.sol";
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+import {IEERC20} from "../../interfaces/euler/IEERC20.sol";
+import {module} from "../../interfaces/euler/module.sol";
+import {dtBorrow} from "../../interfaces/euler/dtBorrow.sol";
+
+/**
+ * @title Aztec Connect Bridge for the Euler protocol
+ * @notice You can use this contract to deposit collateral and borrow.
+ * @dev Implementation of the IDefiBridge interface for eTokens.
+ */
+
+contract EulerLendingBridge is BridgeBase {
+    
+    using SafeERC20 for IERC20;
