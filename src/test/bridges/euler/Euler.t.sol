3b9ce6de1ba8134b4fab9355912a951a2ccb6f8b
Beno <105245519+YtzBeno@users.noreply.github.com>
2022-07-01 22:09:50 +0200

    Update Euler.t.sol

--- src/test/bridges/euler/Euler.t.sol
+++ src/test/bridges/euler/Euler.t.sol
@@ -1,1 +1,24 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2022 Aztec.
+pragma solidity >=0.8.4;
 
+import {BridgeTestBase} from "./../../aztec/base/BridgeTestBase.sol";
+import {AztecTypes} from "../../../aztec/libraries/AztecTypes.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {ICERC20} from "../../../interfaces/compound/ICERC20.sol";
+import {CompoundBridge} from "../../../bridges/compound/CompoundBridge.sol";
+import {ErrorLib} from "../../../bridges/base/ErrorLib.sol";
+
+contract EulerTest is BridgeTestBase {
+    using SafeERC20 for IERC20;
+
+    struct Balances {
+        uint256 underlyingBefore;
+        uint256 underlyingMid;
+        uint256 underlyingEnd;
+        uint256 cBefore;
+        uint256 cMid;
+        uint256 cEnd;
+    }
