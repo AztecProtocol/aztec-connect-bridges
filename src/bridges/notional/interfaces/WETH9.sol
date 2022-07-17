a735938cf3bd0648a4d513df009bfa824abb2e5e
Hao Wang <haowang@college.harvard.edu>
2022-07-17 10:35:39 -0400

    add notional

--- src/bridges/notional/interfaces/WETH9.sol
+++ src/bridges/notional/interfaces/WETH9.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 pragma solidity >=0.8.4;
 =======
 pragma solidity >=0.7.6;
@@ -8,10 +9,14 @@ pragma solidity >=0.7.6;
 =======
 pragma solidity >=0.8.4;
 >>>>>>> 70661815 (add notional bridge)
+=======
+pragma solidity >=0.7.6;
+>>>>>>> ec73562e (add notional)
 
 interface WETH9 {
     function deposit() external payable;
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
     function withdraw(uint256 _wad) external;
@@ -27,4 +32,9 @@ interface WETH9 {
 
     function transfer(address _dst, uint _wad) external returns (bool);
 >>>>>>> 70661815 (add notional bridge)
+=======
+    function withdraw(uint256 wad) external;
+
+    function transfer(address dst, uint wad) external returns (bool);
+>>>>>>> ec73562e (add notional)
 }
