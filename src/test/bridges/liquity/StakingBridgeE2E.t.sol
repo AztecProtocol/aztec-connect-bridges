32ad225f08c12d5cc13573c97f1c35accf454d4b
benesjan <janbenes1234@gmail.com>
2022-07-21 10:06:05 +0200

    test: improvements of StakingBridgeE2E

--- src/test/bridges/liquity/StakingBridgeE2E.t.sol
+++ src/test/bridges/liquity/StakingBridgeE2E.t.sol
@@ -2,20 +2,23 @@
 // Copyright 2022 Aztec.
 pragma solidity >=0.8.4;
 
-import {BridgeTestBase} from "./../../aztec/base/BridgeTestBase.sol";
-import {AztecTypes} from "../../../aztec/libraries/AztecTypes.sol";
-
-// StakingBridge-specific imports
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {ErrorLib} from "../../../bridges/base/ErrorLib.sol";
-import {StakingBridge} from "../../../bridges/liquity/StakingBridge.sol";
 import {Vm} from "forge-std/Vm.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import {AztecTypes} from "../../../aztec/libraries/AztecTypes.sol";
+import {BridgeTestBase} from "./../../aztec/base/BridgeTestBase.sol";
+import {ErrorLib} from "../../../bridges/base/ErrorLib.sol";
+
+import {StakingBridge} from "../../../bridges/liquity/StakingBridge.sol";
 
 contract StakingBridgeE2ETest is BridgeTestBase {
     IERC20 public constant LQTY = IERC20(0x6DEA81C8171D0bA574754EF6F8b412F2Ed88c54D);
+    bytes32 public constant BRIDGE_PROCESSED_EVENT_SIG =
+        keccak256("DefiBridgeProcessed(uint256,uint256,uint256,uint256,uint256,bool,bytes)");
 
     // The reference to the staking bridge
     StakingBridge private bridge;
+    address private stakingContract;
 
     // To store the id of the staking bridge after being added
     uint256 private id;
@@ -25,15 +28,17 @@ contract StakingBridgeE2ETest is BridgeTestBase {
         bridge = new StakingBridge(address(ROLLUP_PROCESSOR));
         bridge.setApprovals();
 
+        stakingContract = address(bridge.STAKING_CONTRACT());
+
         // Use the label cheatcode to mark the address with "Staking Bridge" in the traces
         vm.label(address(bridge), "StakingBridge");
-        vm.label(address(bridge.STAKING_CONTRACT()), "LQTYStakingContract");
+        vm.label(stakingContract, "LQTYStakingContract");
 
         // Impersonate the multi-sig to add a new bridge
         vm.startPrank(MULTI_SIG);
 
-        // List the staking-bridge with a gasLimit of 100K
-        ROLLUP_PROCESSOR.setSupportedBridge(address(bridge), 1000000);
+        // List the staking-bridge with a gasLimit of 350k
+        ROLLUP_PROCESSOR.setSupportedBridge(address(bridge), 350000);
 
         // List the assets with a gasLimit of 100k
         ROLLUP_PROCESSOR.setSupportedAsset(address(LQTY), 100000);
@@ -60,26 +65,34 @@ contract StakingBridgeE2ETest is BridgeTestBase {
         // Compute deposit calldata
         uint256 bridgeCallData = encodeBridgeCallData(id, lqtyAsset, emptyAsset, sbAsset, emptyAsset, 0);
 
+        uint256 stakingBalanceBefore = LQTY.balanceOf(stakingContract);
+
         vm.expectEmit(true, true, false, true);
         emit DefiBridgeProcessed(bridgeCallData, getNextNonce(), _depositAmount, _depositAmount, 0, true, "");
 
         sendDefiRollup(bridgeCallData, _depositAmount);
 
+        assertGe(
+            LQTY.balanceOf(stakingContract) - stakingBalanceBefore,
+            _depositAmount,
+            "Staking contract balance didn't rise enough"
+        );
+
         // 2nd WITHDRAWAL
         // Compute withdrawal calldata
         bridgeCallData = encodeBridgeCallData(id, sbAsset, emptyAsset, lqtyAsset, emptyAsset, 0);
+
+        uint256 processorBalanceBefore = LQTY.balanceOf(address(ROLLUP_PROCESSOR));
 
         vm.recordLogs();
         sendDefiRollup(bridgeCallData, _depositAmount);
         Vm.Log[] memory logs = vm.getRecordedLogs();
 
-        bytes32 eventSig = keccak256("DefiBridgeProcessed(uint256,uint256,uint256,uint256,uint256,bool,bytes)");
-
         uint256 numEventsFound = 0;
         Vm.Log memory defiBridgeProcessedEvent;
 
         for (uint256 i; i < logs.length; ++i) {
-            if (logs[i].topics[0] == eventSig) {
+            if (logs[i].topics[0] == BRIDGE_PROCESSED_EVENT_SIG) {
                 ++numEventsFound;
                 defiBridgeProcessedEvent = logs[i];
             }
@@ -95,5 +108,11 @@ contract StakingBridgeE2ETest is BridgeTestBase {
         assertEq(totalInputValue, _depositAmount, "Incorrect input value decoded");
         assertGe(totalOutputValueA, _depositAmount, "Output value not bigger than deposit");
         assertEq(totalOutputValueB, 0, "Output value B is not 0");
+
+        assertGe(
+            LQTY.balanceOf(address(ROLLUP_PROCESSOR)) - processorBalanceBefore,
+            _depositAmount,
+            "Rollup processor balance didn't rise enough"
+        );
     }
 }
