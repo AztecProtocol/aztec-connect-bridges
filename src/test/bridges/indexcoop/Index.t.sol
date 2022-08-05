380788fafa60189e0c18b3502756dfd01eaadc8b
Tajobin <tajobin@protonmail.com>
2022-08-05 04:33:17 +0200

    Test that attempt to use 0.3 pool reverts

--- src/test/bridges/indexcoop/Index.t.sol
+++ src/test/bridges/indexcoop/Index.t.sol
@@ -412,6 +412,24 @@ contract IndexTest is BridgeTestBase {
         vm.stopPrank();
     }
 
+    // Test using 0.3% pool with no liqduiity
+    function testEmptyUniPool() public{
+        uint256 _depositAmount = 1 ether;
+        uint64 flowSelector = 3; //Select pool with no liqduiity
+        uint64 maxSlipAux = 9900;
+        uint64 oracleLimit = 9900; 
+        uint64 auxData = _encodeData(flowSelector, maxSlipAux, oracleLimit);
+
+        bytes4 emptyByte4;
+        bytes memory emptyBytes;
+        _buySetExpectRevert(_depositAmount, auxData, emptyBytes, emptyByte4);
+
+        maxSlipAux = 9900;
+        oracleLimit = 8000; 
+        auxData = _encodeData(flowSelector, maxSlipAux, oracleLimit);
+        _sellSetExpectRevert(_depositAmount, auxData, emptyBytes, emptyByte4);
+    }
+
     /* 
         Inputs that are too small will result in a loss of precision
         in flashloan calculations. In getAmountBasedOnRedeem() debtOWned
