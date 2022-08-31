e8dd80f1bb73801c25edf593eaa8809433078e71
LHerskind <lasse.herskind@gmail.com>
2022-08-31 13:28:08 +0000

    update aggregate deployment

--- src/deployment/AggregateDeployment.s.sol
+++ src/deployment/AggregateDeployment.s.sol
@@ -2,7 +2,7 @@
 // Copyright 2022 Aztec.
 pragma solidity >=0.8.4;
 
-import {Script} from "forge-std/Script.sol";
+import {Test} from "forge-std/Test.sol";
 
 import {CompoundDeployment} from "./compound/CompoundDeployment.s.sol";
 import {CurveDeployment} from "./curve/CurveDeployment.s.sol";
@@ -15,19 +15,21 @@ import {YearnDeployment} from "./yearn/YearnDeployment.s.sol";
 /**
  * A helper script that allow easy deployment of multiple bridges
  * `deployAndListAll()` should contain all bridges with deployment scripts
- * `deployAndListPartial()` should contain the bridges that  should be deployed to devnet
- * The two helpers will probably overlap, but it is not required.
  */
-contract AggregateDeployment is Script {
-    function deployAndListPartial() public {}
-
+contract AggregateDeployment is Test {
     function deployAndListAll() public {
-        (new CurveDeployment()).deployAndList();
-        (new YearnDeployment()).deployAndList();
-        (new ERC4626Deployment()).deployAndList();
-        (new CompoundDeployment()).deployAndList();
-        (new DonationDeployment()).deployAndList();
-        (new LiquityTroveDeployment()).deployAndList();
-        (new UniswapDeployment()).deployAndList();
+        emit log("--- Curve ---");
+        {
+            CurveDeployment cDeploy = new CurveDeployment();
+            cDeploy.setUp();
+            cDeploy.deployAndList();
+        }
+        
+        emit log("--- Yearn ---");
+        {
+            YearnDeployment yDeploy = new YearnDeployment();
+            yDeploy.setUp();
+            yDeploy.deployAndList();
+        }
     }
 }
