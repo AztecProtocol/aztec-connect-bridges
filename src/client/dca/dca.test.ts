cfdb454f2ccd5417192b37469d4c55acfa0ce2bd
LHerskind <lasse.herskind@gmail.com>
2022-09-07 10:38:53 +0000

    initial dca bridge data class + test

---
+++ src/client/dca/dca.test.ts
@@ -0,0 +1,170 @@
+import { DCABridgeData } from "./dca";
+import { BiDCABridge, BiDCABridge__factory, IERC20__factory } from "../../../typechain-types";
+import { AztecAsset, AztecAssetType } from "../bridge-data";
+import { BigNumber } from "ethers";
+import { EthAddress } from "@aztec/barretenberg/address";
+
+jest.mock("../aztec/provider", () => ({
+  createWeb3Provider: jest.fn(),
+}));
+
+type Mockify<T> = {
+  [P in keyof T]: jest.Mock | any;
+};
+
+describe("DCA bridge data", () => {
+  let dcaBridgeData: DCABridgeData;
+  let dcaBridgeContract: Mockify<BiDCABridge>;
+
+  let ethAsset: AztecAsset;
+  let daiAsset: AztecAsset;
+  let emptyAsset: AztecAsset;
+
+  const createDCABridge = (dcaBridge: BiDCABridge = dcaBridgeContract as any) => {
+    BiDCABridge__factory.connect = () => dcaBridge as any;
+    return DCABridgeData.create({} as any, EthAddress.ZERO);
+  };
+
+  beforeAll(() => {
+    ethAsset = {
+      id: 0,
+      assetType: AztecAssetType.ETH,
+      erc20Address: EthAddress.ZERO,
+    };
+    daiAsset = {
+      id: 1,
+      assetType: AztecAssetType.ERC20,
+      erc20Address: EthAddress.random(),
+    };
+    emptyAsset = {
+      id: 0,
+      assetType: AztecAssetType.NOT_USED,
+      erc20Address: EthAddress.ZERO,
+    };
+  });
+
+  it("get interaction present value", async () => {
+    type DCA = {
+      amount: BigNumber;
+      start: number;
+      end: number;
+      aToB: boolean;
+    };
+
+    type Tick = {
+      availableA: BigNumber;
+      availableB: BigNumber;
+      poke: number;
+      aToBSubTick: {
+        sold: BigNumber;
+        bought: BigNumber;
+      };
+      bToASubTick: {
+        sold: BigNumber;
+        bought: BigNumber;
+      };
+      priceOfAInB: BigNumber;
+      priceTime: number;
+    };
+
+    const dcas: {
+      [key: number]: DCA;
+    } = {
+      0: {
+        amount: BigNumber.from(10).pow(18).mul(7),
+        start: 0,
+        end: 7,
+        aToB: true,
+      },
+    };
+
+    const base: Tick = {
+      availableA: dcas[0].amount.div(7),
+      availableB: BigNumber.from(0),
+      poke: 0,
+      aToBSubTick: {
+        sold: BigNumber.from(0),
+        bought: BigNumber.from(0),
+      },
+      bToASubTick: {
+        sold: BigNumber.from(0),
+        bought: BigNumber.from(0),
+      },
+      priceOfAInB: BigNumber.from(10).pow(18).div(1000),
+      priceTime: 0,
+    };
+
+    const ticks: {
+      [key: number]: Tick;
+    } = {
+      0: base,
+      1: {
+        ...base,
+        availableA: BigNumber.from(0),
+        aToBSubTick: {
+          sold: base.availableA,
+          bought: base.availableA.div(1000),
+        },
+      },
+      2: {
+        ...base,
+        availableA: base.availableA.div(2),
+        aToBSubTick: {
+          sold: base.availableA.div(2),
+          bought: base.availableA.div(2000),
+        },
+      },
+    };
+
+    dcaBridgeContract = {
+      ...dcaBridgeContract,
+      getDCA: jest.fn().mockImplementation((_nonce: number) => dcas[_nonce]),
+      getTick: jest.fn().mockImplementation((_tick: number) => (ticks[_tick] == undefined ? base : ticks[_tick])),
+    };
+
+    dcaBridgeData = createDCABridge(dcaBridgeContract as any);
+    const res = await dcaBridgeData.getInteractionPresentValue(0);
+
+    expect(res[0]).toEqual({
+      assetId: 1,
+      value: (55n * 10n ** 18n) / 10n,
+    });
+    expect(res[1]).toEqual({
+      assetId: 0,
+      value: (15n * 10n ** 18n) / 10000n,
+    });
+  });
+
+  it("get market size", async () => {});
+
+  it("get aux data", async () => {
+    dcaBridgeData = createDCABridge(dcaBridgeContract as any);
+    const result = await dcaBridgeData.getAuxData(emptyAsset, emptyAsset, emptyAsset, emptyAsset);
+    expect(result[0]).toBe(7);
+  });
+
+  it("get expected output", async () => {
+    dcaBridgeData = createDCABridge(dcaBridgeContract as any);
+    const result = await dcaBridgeData.getExpectedOutput(
+      ethAsset,
+      emptyAsset,
+      daiAsset,
+      emptyAsset,
+      0,
+      10n * 10n ** 18n,
+    );
+    expect(result[0]).toBe(0n);
+  });
+
+  it("get underlying amount", async () => {
+    dcaBridgeData = createDCABridge(dcaBridgeContract as any);
+    await expect(dcaBridgeData.getUnderlyingAmount(ethAsset, 10n * 10n ** 18n)).rejects.toEqual(
+      new Error("Not useful information in this bridge"),
+    );
+  });
+
+  it("get apr", async () => {
+    dcaBridgeData = createDCABridge(dcaBridgeContract as any);
+    expect(await dcaBridgeData.getAPR(ethAsset)).toEqual(0);
+  });
+});
