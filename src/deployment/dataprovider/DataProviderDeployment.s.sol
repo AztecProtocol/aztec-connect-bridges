6af64840012aa897171985dcbe1955b3932b8261
LHerskind <lasse.herskind@gmail.com>
2022-09-09 15:44:10 +0000

    remove `createSelectFork` cheatcode

--- src/deployment/dataprovider/DataProviderDeployment.s.sol
+++ src/deployment/dataprovider/DataProviderDeployment.s.sol
@@ -17,43 +17,45 @@ contract DataProviderDeployment is BaseDeployment {
         return address(provider);
     }
 
-    function listBridge(
-        address _provider,
-        uint256 _bridgeAddressId,
-        string memory _tag
-    ) public {
-        vm.broadcast();
-        DataProvider(_provider).addBridge(_bridgeAddressId, _tag);
-        emit log_named_uint(string(abi.encodePacked("[Bridge] Listed ", _tag, " at")), _bridgeAddressId);
-    }
-
-    function listAsset(
-        address _provider,
-        uint256 _assetId,
-        string memory _tag
-    ) public {
-        vm.broadcast();
-        DataProvider(_provider).addAsset(_assetId, _tag);
-        emit log_named_uint(string(abi.encodePacked("[Asset]  Listed ", _tag, " at")), _assetId);
-    }
+    function read() public {}
 
     function deployAndListMany() public {}
 
     function deployAndListDevNet() public {
         address provider = deploy();
 
-        listAsset(provider, 1, "Dai");
-        listAsset(provider, 2, "WSTETH");
-        listAsset(provider, 3, "vyDai");
-        listAsset(provider, 4, "vyWeth");
+        _listAsset(provider, 1, "Dai");
+        _listAsset(provider, 2, "WSTETH");
+        _listAsset(provider, 3, "vyDai");
+        _listAsset(provider, 4, "vyWeth");
 
-        listBridge(provider, 1, "Element Fixed Yield");
-        listBridge(provider, 6, "Curve swap steth pool");
-        listBridge(provider, 7, "Yearn Deposit");
-        listBridge(provider, 8, "Yearn Withdraw");
-        listBridge(provider, 9, "DCA DAI/ETH 7 Days");
+        _listBridge(provider, 1, "Element Fixed Yield");
+        _listBridge(provider, 6, "Curve swap steth pool");
+        _listBridge(provider, 7, "Yearn Deposit");
+        _listBridge(provider, 8, "Yearn Withdraw");
+        _listBridge(provider, 9, "DCA DAI/ETH 7 Days");
 
         DataProvider.BridgeData memory bridge = DataProvider(provider).getBridge("Yearn Withdraw");
         emit log_named_uint(bridge.label, bridge.bridgeAddressId);
+    }
+
+    function _listBridge(
+        address _provider,
+        uint256 _bridgeAddressId,
+        string memory _tag
+    ) internal {
+        vm.broadcast();
+        DataProvider(_provider).addBridge(_bridgeAddressId, _tag);
+        emit log_named_uint(string(abi.encodePacked("[Bridge] Listed ", _tag, " at")), _bridgeAddressId);
+    }
+
+    function _listAsset(
+        address _provider,
+        uint256 _assetId,
+        string memory _tag
+    ) internal {
+        vm.broadcast();
+        DataProvider(_provider).addAsset(_assetId, _tag);
+        emit log_named_uint(string(abi.encodePacked("[Asset]  Listed ", _tag, " at")), _assetId);
     }
 }
