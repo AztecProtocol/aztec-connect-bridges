191bc8c07565b6509148feba8ab3374d8cd4d8f7
LHerskind <lasse.herskind@gmail.com>
2022-09-30 07:50:21 +0000

    feat: update aggregate to list vault on erc4626 as well

--- src/deployment/AggregateDeployment.s.sol
+++ src/deployment/AggregateDeployment.s.sol
@@ -12,6 +12,7 @@ import {CompoundDeployment} from "./compound/CompoundDeployment.s.sol";
 import {CurveDeployment} from "./curve/CurveDeployment.s.sol";
 import {DonationDeployment} from "./donation/DonationDeployment.s.sol";
 import {ERC4626Deployment} from "./erc4626/ERC4626Deployment.s.sol";
+import {ERC4626Lister} from "./erc4626/ERC4626Lister.s.sol";
 import {LiquityTroveDeployment} from "./liquity/LiquityTroveDeployment.s.sol";
 import {UniswapDeployment} from "./uniswap/UniswapDeployment.s.sol";
 import {YearnDeployment} from "./yearn/YearnDeployment.s.sol";
@@ -21,6 +22,8 @@ import {DCADeployment} from "./dca/DCADeployment.s.sol";
  * A helper script that allow easy deployment of multiple bridges
  */
 contract AggregateDeployment is BaseDeployment {
+    address internal erc4626Bridge;
+
     function deployAndListAll() public {
         emit log("--- Curve ---");
         {
@@ -46,22 +49,28 @@ contract AggregateDeployment is BaseDeployment {
         {
             ERC4626Deployment deploy = new ERC4626Deployment();
             deploy.setUp();
-            deploy.deployAndList();
+            erc4626Bridge = deploy.deployAndList();
         }
 
         emit log("--- Euler ---");
         {
+            ERC4626Lister lister = new ERC4626Lister();
+            lister.setUp();
+
             address erc4626EulerWETH = 0x3c66B18F67CA6C1A71F829E2F6a0c987f97462d0;
             uint256 weWethAssetId = listAsset(erc4626EulerWETH, 55000);
             emit log_named_uint("ERC4626 euler weth id", weWethAssetId);
+            lister.listVault(erc4626Bridge, erc4626EulerWETH);
 
             address erc4626EulerWSTETH = 0x60897720AA966452e8706e74296B018990aEc527;
             uint256 wewstEthAssetId = listAsset(erc4626EulerWSTETH, 55000);
             emit log_named_uint("ERC4626 euler wstEth id", wewstEthAssetId);
+            lister.listVault(erc4626Bridge, erc4626EulerWSTETH);
 
             address erc4626EulerDai = 0x4169Df1B7820702f566cc10938DA51F6F597d264;
             uint256 wedaiAssetId = listAsset(erc4626EulerDai, 55000);
             emit log_named_uint("ERC4626 euler dai id", wedaiAssetId);
+            lister.listVault(erc4626Bridge, erc4626EulerDai);
         }
 
         emit log("--- DCA ---");
---
+++ src/deployment/erc4626/ERC4626Lister.s.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2022 Aztec.
+pragma solidity >=0.8.4;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {BaseDeployment} from "../base/BaseDeployment.s.sol";
+import {ERC4626Bridge} from "../../bridges/erc4626/ERC4626Bridge.sol";
+import {IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
+import {IRollupProcessor} from "../../aztec/interfaces/IRollupProcessor.sol";
+
+contract ERC4626Lister is BaseDeployment {
+    function listVault(address _bridge, address _vault) public {
+        ERC4626Bridge bridge = ERC4626Bridge(payable(_bridge));
+
+        IERC4626 vault = IERC4626(_vault);
+        IERC20 asset = IERC20(vault.asset());
+
+        if (vault.allowance(address(bridge), ROLLUP_PROCESSOR) == type(uint256).max) {
+            return;
+        }
+        if (asset.allowance(address(bridge), ROLLUP_PROCESSOR) == type(uint256).max) {
+            return;
+        }
+        if (asset.allowance(address(bridge), address(vault)) == type(uint256).max) {
+            return;
+        }
+
+        vm.broadcast();
+        bridge.listVault(address(vault));
+
+        emit log_named_string("Listed vault", vault.symbol());
+    }
+}
