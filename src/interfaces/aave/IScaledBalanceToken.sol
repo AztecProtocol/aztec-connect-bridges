36104bb9ca7be724c7d9cedc6f4200427f54a8b8
benesjan <janbenes1234@gmail.com>
2022-10-17 17:06:31 +0200

    chore: removed AAVE and Compound

--- src/interfaces/aave/IScaledBalanceToken.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-// Copyright 2022 Aztec.
-pragma solidity >=0.6.10;
-
-interface IScaledBalanceToken {
-    function scaledBalanceOf(address user) external view returns (uint256);
-
-    function underlying() external view returns (address);
-}
--- src/test/bridges/angle/AngleSLPE2E.t.sol
+++ src/test/bridges/angle/AngleSLPE2E.t.sol
@@ -7,7 +7,6 @@ import {AztecTypes} from "../../../aztec/libraries/AztecTypes.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {ICERC20} from "../../../interfaces/compound/ICERC20.sol";
 import {AngleSLPBridge} from "../../../bridges/angle/AngleSLPBridge.sol";
 import {ErrorLib} from "../../../bridges/base/ErrorLib.sol";
 
