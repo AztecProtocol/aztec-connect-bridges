ddf79e90b0ddb66c53463402ac57472ac43b1305
benesjan <janbenes1234@gmail.com>
2022-10-23 14:18:26 +0200

    chore: not enforcing custom error in BaseDeployment

--- src/deployment/base/BaseDeployment.s.sol
+++ src/deployment/base/BaseDeployment.s.sol
@@ -66,6 +66,7 @@ abstract contract BaseDeployment is Test {
         }
 
         (ROLLUP_PROCESSOR, TO_IMPERSONATE) = getRollupProcessorAndLister();
+        /* solhint-disable custom-error-over-require */
         require(ROLLUP_PROCESSOR != address(0), "RollupProcessor address resolved to 0");
         emit log_named_address("Rollup at", ROLLUP_PROCESSOR);
     }
