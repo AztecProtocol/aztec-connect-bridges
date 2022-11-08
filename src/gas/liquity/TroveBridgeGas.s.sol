e3e279481bd0eab67efa254d8ec633abf62c8ddd
Jan Beneš <janbenes1234@gmail.com>
2022-11-08 11:37:24 -0600

    Opening a trove in the deployment script (#278)

--- src/gas/liquity/TroveBridgeGas.s.sol
+++ src/gas/liquity/TroveBridgeGas.s.sol
@@ -9,9 +9,6 @@ import {ISubsidy} from "../../aztec/interfaces/ISubsidy.sol";
 
 import {LiquityTroveDeployment, BaseDeployment} from "../../deployment/liquity/LiquityTroveDeployment.s.sol";
 import {GasBase} from "../base/GasBase.sol";
-import {IHintHelpers} from "../../interfaces/liquity/IHintHelpers.sol";
-import {ITroveManager} from "../../interfaces/liquity/ITroveManager.sol";
-import {ISortedTroves} from "../../interfaces/liquity/ISortedTroves.sol";
 
 interface IRead {
     function defiBridgeProxy() external view returns (address);
@@ -20,15 +17,6 @@ interface IRead {
 contract TroveBridgeMeasure is LiquityTroveDeployment {
     ISubsidy private constant SUBSIDY = ISubsidy(0xABc30E831B5Cc173A9Ed5941714A7845c909e7fA);
     address private constant BENEFICIARY = address(uint160(uint256(keccak256(abi.encodePacked("_BENEFICIARY")))));
-
-    IHintHelpers internal constant HINT_HELPERS = IHintHelpers(0xE84251b93D9524E0d2e621Ba7dc7cb3579F997C0);
-    ITroveManager public constant TROVE_MANAGER = ITroveManager(0xA39739EF8b0231DbFA0DcdA07d7e29faAbCf4bb2);
-    ISortedTroves internal constant SORTED_TROVES = ISortedTroves(0x8FdD3fbFEb32b28fb73555518f8b361bCeA741A6);
-
-    uint256 internal constant OWNER_ETH_BALANCE = 5 ether;
-    uint64 internal constant MAX_FEE = 5e16; // Slippage protection: 5%
-    // From LiquityMath.sol
-    uint256 internal constant NICR_PRECISION = 1e20;
 
     GasBase internal gasBase;
     TroveBridge internal bridge;
@@ -80,7 +68,7 @@ contract TroveBridgeMeasure is LiquityTroveDeployment {
     }
 
     function measureETH() public {
-        _openTrove();
+        openTrove(address(bridge));
         vm.broadcast();
         address(gasBase).call{value: 2 ether}("");
         emit log_named_uint("ETH balance of gasBase", address(gasBase).balance);
@@ -136,20 +124,5 @@ contract TroveBridgeMeasure is LiquityTroveDeployment {
             "Subsidy was not claimed during repayment"
         );
         emit log_named_uint("Claimable subsidy after repayment", claimableSubsidyAfterRepayment);
-    }
-
-    function _openTrove() internal {
-        uint256 amtToBorrow = bridge.computeAmtToBorrow(OWNER_ETH_BALANCE);
-        uint256 nicr = (OWNER_ETH_BALANCE * NICR_PRECISION) / amtToBorrow;
-
-        // The following is Solidity implementation of https://github.com/liquity/dev#opening-a-trove
-        uint256 numTrials = 15;
-        uint256 randomSeed = 42;
-        (address approxHint, , ) = HINT_HELPERS.getApproxHint(nicr, numTrials, randomSeed);
-        (address upperHint, address lowerHint) = SORTED_TROVES.findInsertPosition(nicr, approxHint, approxHint);
-
-        // Open the trove
-        vm.broadcast();
-        bridge.openTrove{value: OWNER_ETH_BALANCE}(upperHint, lowerHint, MAX_FEE);
     }
 }
