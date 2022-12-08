055c3996eaf2df000efb99423612ac08dd4f3054
Josh Crites <critesjosh@gmail.com>
2022-12-08 11:09:22 -0500

    natspec comments

--- src/bridges/nft-basic/NftVault.sol
+++ src/bridges/nft-basic/NftVault.sol
@@ -10,7 +10,7 @@ import {AddressRegistry} from "../registry/AddressRegistry.sol";
 
 /**
  * @title Basic NFT Vault for Aztec.
- * @author Aztec Team
+ * @author Josh Crites, (@critesjosh on Github),  Aztec Team
  * @notice You can use this contract to hold your NFTs on Aztec. Whoever holds the corresponding virutal asset note can withdraw the NFT.
  * @dev This bridge demonstrates basic functionality for an NFT bridge. This may be extended to support more features.
  */
@@ -26,9 +26,30 @@ contract NftVault is BridgeBase {
     event NftDeposit(uint256 indexed virtualAssetId, address indexed collection, uint256 indexed tokenId);
     event NftWithdraw(uint256 indexed virtualAssetId, address indexed collection, uint256 indexed tokenId);
 
+    /**
+     * @notice Set the addresses of RollupProcessor.sol and AddressRegistry.sol
+     * @param _rollupProcessor Address of the RollupProcessor.sol
+     * @param _registry Address of the AddressRegistry.sol
+     */
     constructor(address _rollupProcessor, address _registry) BridgeBase(_rollupProcessor) {
         registry = AddressRegistry(_registry);
     }
+
+    /**
+     * @notice Function for the first step of a NFT deposit, or a NFT withdrawal.
+     * @dev This method can only be called from the RollupProcessor.sol. The first step of the 
+     * deposit flow returns a virutal asset note that will represent the NFT on Aztec. After the 
+     * virutal asset note is received on Aztec, the user calls matchDeposit which deposits the NFT
+     * into Aztec and matches it with the virtual asset. When the virutal asset is sent to this function
+     * it is burned and the NFT is sent to the user (withdraw).
+     *
+     * @param _inputAssetA - ETH (Deposit) or VIRTUAL (Withdrawal)
+     * @param _outputAssetA - VIRTUAL (Deposit) or 0 ETH (Withdrawal)
+     * @param _totalInputValue - must be 1 wei (Deposit) or 1 VIRTUAL (Withdrawal)
+     * @param _auxData - corresponds to the Ethereum address id in the AddressRegistry.sol for withdrawals
+     * @return outputValueA - 1 VIRTUAL asset (Deposit) or 0 ETH (Withdrawal)
+     *
+     */
 
     function convert(
         AztecTypes.AztecAsset calldata _inputAssetA,
@@ -80,6 +101,17 @@ contract NftVault is BridgeBase {
             return (0, 0, false);
         }
     }
+
+    /**
+     * @notice Function for the second step of a NFT deposit.
+     * @dev This method is called by an Ethereum L1 account that owns the NFT to deposit. 
+     * The user must approve this bridge contract to transfer the users NFT before this function
+     * is called. This function assumes the NFT contract complies with the ERC721 standard.
+     *
+     * @param _virtualAssetId - the virutal asset id of the note returned in the deposit step of the convert function
+     * @param _collection - collection address of the NFT
+     * @param _tokenId - the token id of the NFT
+     */
 
     function matchDeposit(uint256 _virtualAssetId, address _collection, uint256 _tokenId) external {
         require(tokens[_virtualAssetId].collection == address(0x0), "Asset registered");
--- src/bridges/registry/AddressRegistry.sol
+++ src/bridges/registry/AddressRegistry.sol
@@ -8,7 +8,7 @@ import {BridgeBase} from "../base/BridgeBase.sol";
 
 /**
  * @title Aztec Address Registry.
- * @author Aztec Team
+ * @author Josh Crites (@critesjosh on Github), Aztec team
  * @notice This contract can be used to anonymously register an ethereum address with an id.
  *         This is useful for reducing the amount of data required to pass an ethereum address through auxData.
  * @dev Use this contract to lookup ethereum addresses by id.
@@ -24,6 +24,20 @@ contract AddressRegistry is BridgeBase {
      * @param _rollupProcessor Address of rollup processor
      */
     constructor(address _rollupProcessor) BridgeBase(_rollupProcessor) {}
+
+    /**
+     * @notice Function for getting VIRTUAL assets (step 1) to register an address and registering an address (step 2).
+     * @dev This method can only be called from the RollupProcessor.sol. The first step to register an address is for a user to
+     * get the type(uint160).max value of VIRTUAL assets back from the bridge. The second step is for the user 
+     * to send an amount of VIRTUAL assets back to the bridge. The amount that is sent back is equal to number of the
+     * ethereum address that is being registered (e.g. uint160(0x2e782B05290A7fFfA137a81a2bad2446AD0DdFEB)).
+     *
+     * @param _inputAssetA - ETH (step 1) or VIRTUAL (step 2)
+     * @param _outputAssetA - VIRTUAL (steps 1 and 2)
+     * @param _totalInputValue - must be 1 wei (ETH) (step 1) or address value (step 2)
+     * @return outputValueA - type(uint160).max (step 1) or 0 VIRTUAL (step 2)
+     *
+     */
 
     function convert(
         AztecTypes.AztecAsset calldata _inputAssetA,
@@ -61,6 +75,15 @@ contract AddressRegistry is BridgeBase {
             revert("invalid input/output types");
         }
     }
+
+    /**
+     * @notice Function for registering an address from Ethereum.
+     * @dev This function can be called directly from another Ethereum account. This can be done in
+     * one step, in one transaction. Coming from Ethereum directly, this method is not as privacy
+     * preserving as registering an address through the bridge.
+     * 
+     * @param _to - ETH (step 1) or VIRTUAL (step 2)
+     */
 
     function registerWithdrawAddress(address _to) external returns (uint256) {
         addressCount++;
