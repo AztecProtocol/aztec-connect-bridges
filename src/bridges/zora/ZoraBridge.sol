e84fa226ec151139bc67af80b8be95f67beda55f
Michael Neuder <mikeneuder@google.com>
2022-12-20 22:07:39 -0500

    First pass

--- src/bridges/zora/ZoraBridge.sol
+++ src/bridges/zora/ZoraBridge.sol
@@ -7,70 +7,74 @@ import {AztecTypes} from "rollup-encoder/libraries/AztecTypes.sol";
 import {ErrorLib} from "../base/ErrorLib.sol";
 import {BridgeBase} from "../base/BridgeBase.sol";
 
-/*
-library AztecTypes {
-    enum AztecAssetType {
-        NOT_USED,
-        ETH,
-        ERC20,
-        ERC721, <- need to add this.
-        VIRTUAL
-    }
-
-    struct AztecAsset {
-        uint256 id;
-        address erc20Address;     <- need to rename this (no longer just erc-20).
-        AztecAssetType assetType;
-    }
+// Get function definition from: https://github.com/ourzora/v3/blob/1d4c0c951ccd5c1d446283ce6fef3757ad97b804/contracts/modules/Asks/V1.1/AsksV1_1.sol#L302.
+contract ZoraAsk {
+    function fillAsk(
+        address _tokenContract,
+        uint256 _tokenId,
+        address _fillCurrency,
+        uint256 _fillAmount,
+        address _finder
+    ) external {}
 }
-*/
 
-contract Zora is BridgeBase {
+contract ZoraBridge is BridgeBase {
+    struct ZoraNFT {
+        address collection;
+        uint256 tokenId;
+    }
+
+    uint64 internal zoraNFTCount;
+    mapping(uint64 => ZoraNFT) internal zoraNFTs;
+    ZoraAsk internal za;
+
+    /**
+     * @notice Set the addresses of RollupProcessor
+     * @param _rollupProcessor Address of the RollupProcessor
+     */
+    constructor(address _rollupProcessor, address _zora) BridgeBase(_rollupProcessor) {
+        za = ZoraAsk(_zora);
+    }
+
+    // Converts ETH into a virtual token by filling an ask on the Zora contract.
     function convert(
         AztecTypes.AztecAsset calldata _inputAssetA,
-        AztecTypes.AztecAsset calldata _inputAssetB,
+        AztecTypes.AztecAsset calldata,
         AztecTypes.AztecAsset calldata _outputAssetA,
         AztecTypes.AztecAsset calldata,
         uint256 _totalInputValue,
         uint256,
         uint64 _auxData,
         address
-    ) external payable override (BridgeBase) onlyRollup returns (uint256 outputValueA, uint256, bool) {
-        /*
-        To fill an ask, we need Bob to authorize us to send ERC-20 tokens to Zora with this contract as the recipient.
-        _inputAssetA:  ERC-20 token to pay for the Ask.
-        _inputAssetB:  ERC-721 token the we want to fill the Ask of.
-        _outputAssetA: Virtual token that corresponds to the ERC-721 token now owned by Aztec.
+    ) external payable override (BridgeBase) onlyRollup returns (uint256 outputValueA, uint256 outputValueB, bool async) {
+        // Add error handling for input/output types.
 
-        Now construct the fillAsk request: https://docs.zora.co/docs/smart-contracts/modules/Asks/zora-v3-asks-v1.1#fillask,
-        and call it on the zora contract.
-        function fillAsk(
-            address _tokenContract, <- _inputAssetB.address
-            uint256 _tokenId,       <- _inputAssetB.id
-            address _fillCurrency,  <- _inputAssetA.address
-            uint256 _fillAmount,    <- _totalInputValue
-            address _finder         <- 0x0000000000000000000000000000000000000000
-        ) 
+        if (_inputAssetA.assetType == AztecTypes.AztecAssetType.ETH &&
+            _outputAssetA.assetType == AztecTypes.AztecAssetType.VIRTUAL) {
 
-        Then we utilize the NFTVault bridge.
-        virtualToken, _, _ = NftVault.convert(
-            AztecTypes.AztecAsset calldata _inputAssetA,  <- ETH 
-            AztecTypes.AztecAsset calldata,
-            AztecTypes.AztecAsset calldata _outputAssetA, <- VIRTUAL
-            AztecTypes.AztecAsset calldata,
-            uint256 _totalInputValue,                     <- 1 WEI
-            uint256,
-            uint64,
-            address
-        )
-        matchDeposit(
-            uint256 _virtualAssetId, <- vitualToken.id
-            address _collection,     <- inputAssetB.address
-            uint256 _tokenId         <- inputAssetB.id
-        )
+            // Look up the NFT details using the _auxData.
+            ZoraNFT memory nft = zoraNFTs[_auxData];
 
-        Lastly, we return the virtual token.
-        return (1, 0, false)
-        */
+            // Add error handling for if the NFT is not in the map. 
+            za.fillAsk(
+                nft.collection,
+                nft.tokenId,
+                address(0x0),     // 0 address to indicate this sale is in ETH.
+                _totalInputValue, // Use the total input value to specify how much ETH to fill the ask with.
+                address(0x0)      // Leave the finder address empty.
+            );
+            // Return a virtual token.
+            return (1, 0, false);
+        }
+    }
+
+    // A function to register the details of a Zora NFT.
+    function registerZoraNFT(address _collection, uint256 _tokenId) public returns (uint64) {
+        uint64 newIndex = zoraNFTCount++;
+        zoraNFTs[newIndex] = ZoraNFT({
+            collection: _collection,
+            tokenId: _tokenId
+        });
+        return newIndex;
     }
 }
