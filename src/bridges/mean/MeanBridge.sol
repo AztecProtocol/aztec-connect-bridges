e89a3c9ad502da2a65b4869a1b1cf7835930d59c
Nicolas Chamo <nicolas@chamo.com.ar>
2023-01-30 21:06:15 -0300

    feat: add max approve

--- src/bridges/mean/MeanBridge.sol
+++ src/bridges/mean/MeanBridge.sol
@@ -2,12 +2,15 @@
 // Copyright 2022 Aztec
 pragma solidity >=0.8.4;
 
+import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {Ownable2Step} from "@openzeppelin/contracts/access/Ownable2Step.sol";
 import {AztecTypes} from "rollup-encoder/libraries/AztecTypes.sol";
 import {IRollupProcessor} from "rollup-encoder/interfaces/IRollupProcessor.sol";
 import {IDCAHub} from "../../interfaces/mean/IDCAHub.sol";
+import {ITransformerRegistry} from "../../interfaces/mean/ITransformerRegistry.sol";
+import {ITransformer} from "../../interfaces/mean/ITransformer.sol";
 import {IWETH} from "../../interfaces/IWETH.sol";
 import {ErrorLib} from "../base/ErrorLib.sol";
 import {BridgeBase} from "../base/BridgeBase.sol";
@@ -23,6 +26,7 @@ contract MeanBridge is BridgeBase, Ownable2Step {
 
     IWETH public constant WETH = IWETH(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
     IDCAHub public immutable DCA_HUB;
+    ITransformerRegistry public immutable TRANSFORMER_REGISTRY;
     address private immutable THIS_ADDRESS;
 
     /**
@@ -31,13 +35,37 @@ contract MeanBridge is BridgeBase, Ownable2Step {
      * @param _owner The account that will own the bridge
      * @param _rollupProcessor Address of rollup processor
      */
-    constructor(IDCAHub _hub, address _owner, address _rollupProcessor) BridgeBase(_rollupProcessor) {
+    constructor(IDCAHub _hub, ITransformerRegistry _transformerRegistry, address _owner, address _rollupProcessor) BridgeBase(_rollupProcessor) {
         _transferOwnership(_owner);
         DCA_HUB = _hub;
+        TRANSFORMER_REGISTRY = _transformerRegistry;
         THIS_ADDRESS = address(this);
     }
 
     // Note: we need to be able to receive ETH to deposit as WETH
     receive() external payable {}
+
+    /**
+     * @notice Set all the necessary approvals for the DCA hub, the transformer registry and the rollup processor
+     * @param _tokens - The tokens to approve
+     */
+    function maxApprove(IERC20[] calldata _tokens) external {
+        for (uint i; i < _tokens.length; ) {
+            IERC20 _token = _tokens[i];
+            _maxApprove(_token, address(DCA_HUB));
+            _maxApprove(_token, address(TRANSFORMER_REGISTRY));
+            _maxApprove(_token, address(ROLLUP_PROCESSOR));
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    function _maxApprove(IERC20 _token, address _target) internal {
+        // Using safeApprove(...) instead of approve(...) and first setting the allowance to 0 because underlying
+        // can be Tether
+        IERC20(_token).safeApprove(_target, 0);
+        IERC20(_token).safeApprove(_target, type(uint256).max);
+    }
 
 }
--- src/test/bridges/mean/MeanBridgeUnit.t.sol
+++ src/test/bridges/mean/MeanBridgeUnit.t.sol
@@ -8,12 +8,14 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {MeanBridge} from "../../../bridges/mean/MeanBridge.sol";
 import {ErrorLib} from "../../../bridges/base/ErrorLib.sol";
 import {IDCAHub} from "../../../interfaces/mean/IDCAHub.sol";
+import {ITransformerRegistry} from "../../../interfaces/mean/ITransformerRegistry.sol";
 
 contract MeanBridgeUnitTest is Test {
     IERC20 public constant WETH = IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
     IERC20 public constant DAI = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
     address public constant OWNER = 0x0000000000000000000000000000000000000001;
     IDCAHub public constant DCA_HUB = IDCAHub(0x0000000000000000000000000000000000000002);
+    ITransformerRegistry public constant TRANSFORMER_REGISTRY = ITransformerRegistry(0x0000000000000000000000000000000000000003);
 
     AztecTypes.AztecAsset internal emptyAsset;
 
@@ -25,19 +27,30 @@ contract MeanBridgeUnitTest is Test {
         rollupProcessor = address(this);
 
         // Deploy a new example bridge
-        bridge = new MeanBridge(DCA_HUB, OWNER, rollupProcessor);
+        bridge = new MeanBridge(DCA_HUB, TRANSFORMER_REGISTRY, OWNER, rollupProcessor);
         vm.label(address(bridge), "MEAN_BRIDGE");
         vm.deal(address(bridge), 0);
 
         // Some more labels
         vm.label(address(DAI), "DAI");
         vm.label(address(DCA_HUB), "DCA_HUB");
+        vm.label(address(TRANSFORMER_REGISTRY), "TRANSFORMER_REGISTRY");
         vm.label(address(OWNER), "OWNER");
     }
 
     function testSetup() public {
         assertEq(address(bridge.DCA_HUB()), address(DCA_HUB));
+        assertEq(address(bridge.TRANSFORMER_REGISTRY()), address(TRANSFORMER_REGISTRY));
         assertEq(bridge.owner(), OWNER);
+    }
+
+    function testMaxApprove() public {
+        IERC20[] memory _tokens = new IERC20[](1);
+        _tokens[0] = DAI;
+        bridge.maxApprove(_tokens);
+        assertEq(DAI.allowance(address(bridge), address(DCA_HUB)), type(uint256).max);
+        assertEq(DAI.allowance(address(bridge), address(TRANSFORMER_REGISTRY)), type(uint256).max);
+        assertEq(DAI.allowance(address(bridge), rollupProcessor), type(uint256).max);
     }
 
 }
