c5a9acd3e3d8460a75eaccc704daecec856bc1da
benesjan <janbenes1234@gmail.com>
2023-02-03 11:27:51 -0600

    feat: Uniswap subsidy

--- src/scripts/SubsidyLogger.sol
+++ src/scripts/SubsidyLogger.sol
@@ -8,6 +8,7 @@ import {AztecTypes} from "rollup-encoder/libraries/AztecTypes.sol";
 import {ISubsidy} from "../aztec/interfaces/ISubsidy.sol";
 import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import {IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
+import {UniswapBridge} from "../bridges/uniswap/UniswapBridge.sol";
 
 /**
  * @title Script which logs either all subsidized or non-subsidized bridges
@@ -36,12 +37,22 @@ contract SubsidyLogger is Test {
         "ERC4626-Wrapped AAVE V2 WETH (wa2WETH)"
     ];
 
-    AztecTypes.AztecAsset internal emptyAsset;
     // @dev if set to true only subsidies which need to be funded get displayed
     bool public onlyEmpty = false;
 
+    AztecTypes.AztecAsset internal emptyAsset;
+    AztecTypes.AztecAsset internal ethAsset;
+    AztecTypes.AztecAsset internal icEthAsset;
+
     function setUp() public {
         onlyEmpty = vm.envBool("ONLY_EMPTY");
+        ethAsset = AztecTypes.AztecAsset({id: 0, erc20Address: address(0), assetType: AztecTypes.AztecAssetType.ETH});
+        icEthAsset = AztecTypes.AztecAsset({
+            id: 14,
+            erc20Address: 0x7C07F7aBe10CE8e33DC6C5aD68FE033085256A84,
+            assetType: AztecTypes.AztecAssetType.ERC20
+        });
+
         emit log_named_uint("Hours for full subsidy", FULL_SUBSIDY_TIME);
         emit log_named_decimal_uint("Gas price used in gwei", ESTIMATION_BASE_FEE, 9);
     }
@@ -51,6 +62,7 @@ contract SubsidyLogger is Test {
         logYearnSubsidies();
         logLiquityTroveSubsidies(0x998650bf01A6424F9B11debd85a29090906cB559); // TB-275
         logLiquityTroveSubsidies(0x646Df2Dc98741a0Ab5798DeAC6Fc62411dA41D96); // TB-400
+        logUniswapSubsidies(ethAsset, icEthAsset);
     }
 
     function logERC4626Subsidies() public {
@@ -112,7 +124,6 @@ contract SubsidyLogger is Test {
     }
 
     function logYearnSubsidies() public {
-        // bridge address asset combination gas usage subsidy
         BridgeBase bridge = BridgeBase(0xE71A50a78CcCff7e20D8349EED295F12f0C8C9eF);
 
         emit log_string("\n");
@@ -161,6 +172,53 @@ contract SubsidyLogger is Test {
 
         ISubsidy.Subsidy memory enterSubsidy = SUBSIDY.getSubsidy(_bridge, enterCriteria);
         ISubsidy.Subsidy memory exitSubsidy = SUBSIDY.getSubsidy(_bridge, exitCriteria);
+
+        if (enterSubsidy.available == 0 || !onlyEmpty) {
+            uint256 gasPerMinute = enterSubsidy.gasUsage / (FULL_SUBSIDY_TIME * 60);
+            emit log_string("========================");
+            emit log_named_uint("enterCriteria", enterCriteria);
+            emit log_named_uint("enterCriteria available", enterSubsidy.available);
+            emit log_named_uint("enterCriteria gasUsage", enterSubsidy.gasUsage);
+            emit log_named_uint("recommended minGasPerMinute", gasPerMinute);
+            uint256 costOfMonth = enterSubsidy.gasUsage * (24 * 30) * ESTIMATION_BASE_FEE / FULL_SUBSIDY_TIME;
+            emit log_named_decimal_uint("cost of fully subsidizing for a month", costOfMonth, 18);
+        }
+
+        if (exitSubsidy.available == 0 || !onlyEmpty) {
+            uint256 gasPerMinute = exitSubsidy.gasUsage / (FULL_SUBSIDY_TIME * 60);
+            emit log_string("========================");
+            emit log_named_uint("exitCriteria", exitCriteria);
+            emit log_named_uint("exitCriteria available", exitSubsidy.available);
+            emit log_named_uint("exitCriteria gasUsage", exitSubsidy.gasUsage);
+            emit log_named_uint("recommended minGasPerMinute", gasPerMinute);
+            uint256 costOfMonth = exitSubsidy.gasUsage * (24 * 30) * ESTIMATION_BASE_FEE / FULL_SUBSIDY_TIME;
+            emit log_named_decimal_uint("cost of fully subsidizing for a month", costOfMonth, 18);
+        }
+
+        emit log_string("========================");
+    }
+
+    function logUniswapSubsidies(AztecTypes.AztecAsset memory _inputAssetA, AztecTypes.AztecAsset memory _outputAssetA)
+        public
+    {
+        // For some reason when I use the actual deployment of the bridge on mainnet I get zero criteria when calling
+        // computeCriteria. I don't know why and don't want to spend time on it now since we are not setting u
+        // the subsidy now. When I use a new deployment of the bridge here it works fine. Probably some issue with
+        // Foundry setting.
+        // BridgeBase bridge = BridgeBase(0x5594808e8A7b44da9D2382E6d72ad50a3e2571E0);
+        UniswapBridge bridge = new UniswapBridge(0xFF1F2B4ADb9dF6FC8eAFecDcbF96A2B351680455);
+
+        emit log_string("\n");
+        emit log_string("=========== Uniswap Bridge =============");
+        emit log_named_address("Bridge address", address(bridge));
+        emit log_named_address("Input asset", _inputAssetA.erc20Address);
+        emit log_named_address("Output asset", _outputAssetA.erc20Address);
+
+        uint256 enterCriteria = bridge.computeCriteria(_inputAssetA, emptyAsset, _outputAssetA, emptyAsset, 0);
+        uint256 exitCriteria = bridge.computeCriteria(_outputAssetA, emptyAsset, _inputAssetA, emptyAsset, 0);
+
+        ISubsidy.Subsidy memory enterSubsidy = SUBSIDY.getSubsidy(address(bridge), enterCriteria);
+        ISubsidy.Subsidy memory exitSubsidy = SUBSIDY.getSubsidy(address(bridge), exitCriteria);
 
         if (enterSubsidy.available == 0 || !onlyEmpty) {
             uint256 gasPerMinute = enterSubsidy.gasUsage / (FULL_SUBSIDY_TIME * 60);
